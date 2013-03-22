9a869d60c0cbc7dc1d0116400d4783adb3ae120a
immars <immars@gmail.com>
2013-03-22 13:32:34 +0800

    test for Join

--- sandbox/prototype/exec/ref/src/test/java/org/apache/drill/exec/ref/TestUtils.java
+++ sandbox/prototype/exec/ref/src/test/java/org/apache/drill/exec/ref/TestUtils.java
@@ -1,12 +1,25 @@
 package org.apache.drill.exec.ref;
 
+import com.google.common.base.Charsets;
+import com.google.common.collect.Queues;
+import com.google.common.io.Files;
 import org.apache.drill.common.config.DrillConfig;
 import org.apache.drill.common.expression.SchemaPath;
+import org.apache.drill.common.logical.LogicalPlan;
+import org.apache.drill.common.util.FileUtils;
+import org.apache.drill.exec.ref.eval.BasicEvaluatorFactory;
 import org.apache.drill.exec.ref.rse.JSONRecordReader;
+import org.apache.drill.exec.ref.rse.RSERegistry;
+import org.codehaus.jackson.node.ArrayNode;
 
-import java.io.ByteArrayInputStream;
-import java.io.IOException;
-import java.io.InputStream;
+import java.io.*;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 public class TestUtils {
   public static RecordIterator jsonToRecordIterator(String schemaPath, String j) throws IOException {
@@ -25,4 +38,23 @@ public class TestUtils {
     }
     return counter;
   }
+
+  /**
+   *
+   * @param resourcePath path for json plan file
+   * @param recordCount expected record count
+   * @throws Exception
+   */
+  public static void assertProduceCount(String resourcePath, int recordCount) throws Exception {
+    DrillConfig config = DrillConfig.create();
+    final BlockingQueue<Object> queue = new ArrayBlockingQueue<Object>(100);
+    config.setSinkQueues(0, queue);
+    LogicalPlan plan = LogicalPlan.parse(config, Files.toString(FileUtils.getResourceAsFile(resourcePath), Charsets.UTF_8));
+    IteratorRegistry ir = new IteratorRegistry();
+    ReferenceInterpreter i = new ReferenceInterpreter(plan, ir, new BasicEvaluatorFactory(ir), new RSERegistry(config));
+    i.setup();
+    Collection<RunOutcome> outcomes = i.run();
+    assertEquals(outcomes.iterator().next().records, recordCount);
+  }
+
 }
