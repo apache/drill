b12c0b155698a3d2ecd5cf3bfdf994fccd65f8d6
Steven Phillips <sphillips@maprtech.com>
2013-12-01 20:06:05 -0800

    DRILL-303: RecordBatchLoader.load always creates new schema

--- exec/java-exec/src/main/java/org/apache/drill/exec/record/RecordBatchLoader.java
+++ exec/java-exec/src/main/java/org/apache/drill/exec/record/RecordBatchLoader.java
@@ -62,10 +62,10 @@ public class RecordBatchLoader implements VectorAccessible, Iterable<VectorWrapp
     this.valueCount = def.getRecordCount();
     boolean schemaChanged = schema == null;
 
-    Map<MaterializedField, ValueVector> oldFields = Maps.newHashMap();
+    Map<FieldDef, ValueVector> oldFields = Maps.newHashMap();
     for(VectorWrapper<?> w : container){
       ValueVector v = w.getValueVector();
-      oldFields.put(v.getField(), v);
+      oldFields.put(v.getField().getDef(), v);
     }
     
     VectorContainer newVectors = new VectorContainer();
@@ -76,15 +76,12 @@ public class RecordBatchLoader implements VectorAccessible, Iterable<VectorWrapp
     for (FieldMetadata fmd : fields) {
       FieldDef fieldDef = fmd.getDef();
       ValueVector v = oldFields.remove(fieldDef);
-      if(v != null){
-        container.add(v);
-        continue;
+      if(v == null) {
+        // if we arrive here, we didn't have a matching vector.
+        schemaChanged = true;
+        MaterializedField m = new MaterializedField(fieldDef);
+        v = TypeHelper.getNewVector(m, allocator);
       }
-
-      // if we arrive here, we didn't have a matching vector.
-      schemaChanged = true;
-      MaterializedField m = new MaterializedField(fieldDef);
-      v = TypeHelper.getNewVector(m, allocator);
       if (fmd.getValueCount() == 0){
         v.clear();
       } else {
