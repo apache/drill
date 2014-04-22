5de76b28a1596ecd675c8f8ef73a48e33b27bb81
Mehant Baid <mehantr@gmail.com>
2014-04-22 16:42:06 -0700

    DRILL-556: Implement the following aggregate functions. stddev() stddev_samp() stddev_pop() variance() var_samp() var_pop()
    Date, Interval aggregate functions

--- exec/java-exec/src/main/java/org/apache/drill/exec/expr/fn/impl/DateUtility.java
+++ exec/java-exec/src/main/java/org/apache/drill/exec/expr/fn/impl/DateUtility.java
@@ -1199,8 +1199,12 @@ public class DateUtility {
     public static final int hoursToMillis = 60 * 60 * 1000;
     public static final int minutesToMillis = 60 * 1000;
     public static final int secondsToMillis = 1000;
+    public static final int monthToStandardDays = 30;
+    public static final int monthsToMillis = 30 * 24 * 60 * 60 * 1000;
+    public static final int daysToStandardMillis = 24 * 60 * 60 * 1000;
 
-    public static int getIndex(String timezone) {
+
+  public static int getIndex(String timezone) {
         return timezoneMap.get(timezone);
     }
 
---
+++ sqlparser/src/test/java/org/apache/drill/jdbc/test/TestDateAggregateFunction.java
@@ -0,0 +1,62 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.drill.jdbc.test;
+
+import java.nio.file.Paths;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+import java.sql.Statement;
+import java.util.concurrent.TimeUnit;
+
+import org.apache.drill.common.util.TestTools;
+import org.apache.drill.exec.store.hive.HiveTestDataGenerator;
+import org.apache.drill.jdbc.Driver;
+import org.apache.drill.jdbc.JdbcTest;
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Ignore;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.TestRule;
+
+import com.google.common.base.Function;
+import com.google.common.base.Stopwatch;
+
+public class TestDateAggregateFunction {
+
+  public static final String WORKING_PATH;
+  static{
+    Driver.load();
+    WORKING_PATH = Paths.get("").toAbsolutePath().toString();
+
+  }
+  @Test
+  public void testAbsDecimalFunction() throws Exception{
+    String query = new String("SELECT max(cast(HIRE_DATE as date)) as MAX_DATE, min(cast(HIRE_DATE as date)) as MIN_DATE" +
+        " FROM `employee.json`");
+
+    JdbcAssert.withFull("cp")
+        .sql(query)
+        .returns(
+                "MAX_DATE=1998-01-01; " +
+                "MIN_DATE=1993-05-01\n"
+        );
+  }
+}
