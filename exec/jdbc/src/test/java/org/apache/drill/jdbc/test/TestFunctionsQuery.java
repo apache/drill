cd4f7267de975f89aef775860532d49220ee8e88
Mehant Baid <mehantr@gmail.com>
2014-07-03 00:09:17 -0700

    DRILL-1098: Add test for trim(), substring() function. Actual fix is in OPTIQ-331

--- exec/jdbc/src/test/java/org/apache/drill/jdbc/test/TestFunctionsQuery.java
+++ exec/jdbc/src/test/java/org/apache/drill/jdbc/test/TestFunctionsQuery.java
@@ -690,4 +690,15 @@ public class TestFunctionsQuery {
         .returns(
             "NEG=-2\n");
   }
+
+  @Test
+  public void testOptiqValidationFunctions() throws Exception {
+    String query = "select trim(first_name) as TRIM_STR, substring(first_name, 2) as SUB_STR " +
+                   "from cp.`employee.json` where employee_id = 1";
+
+    JdbcAssert.withNoDefaultSchema()
+        .sql(query)
+        .returns("TRIM_STR=Sheri; "+
+                 "SUB_STR=heri\n");
+  }
 }
