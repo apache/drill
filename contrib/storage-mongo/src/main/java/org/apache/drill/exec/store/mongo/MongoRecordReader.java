946b2df038ba76d604280a11f073162371d78572
akumarb2010 <akumarb2010@gmail.com>
2014-07-03 21:21:38 +0530

    licence addition

--- contrib/storage-mongo/src/main/java/org/apache/drill/exec/store/mongo/MongoRecordReader.java
+++ contrib/storage-mongo/src/main/java/org/apache/drill/exec/store/mongo/MongoRecordReader.java
@@ -1,3 +1,20 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.apache.drill.exec.store.mongo;
 
 import java.util.List;
@@ -8,11 +25,17 @@ import org.apache.drill.exec.ops.FragmentContext;
 import org.apache.drill.exec.physical.impl.OutputMutator;
 import org.apache.drill.exec.store.RecordReader;
 
-public class MongoRecordReader implements RecordReader {
+import com.mongodb.MongoClient;
 
-  public MongoRecordReader(List<SchemaPath> projectedColumns,
-      FragmentContext context) {
-    
+public class MongoRecordReader implements RecordReader {
+  private static final org.slf4j.Logger logger = org.slf4j.LoggerFactory
+      .getLogger(MongoRecordReader.class);
+  private static final int TARGET_RECORD_COUNT = 4000;
+  private MongoClient mongoClient;
+
+  public MongoRecordReader(MongoSubScan.MongoSubScanSpec mongoSubScanSpec,
+      List<SchemaPath> projectedColumns, FragmentContext context) {
+    mongoClient = mongoSubScanSpec.getMongoClient();
   }
 
   @Override
--- contrib/storage-mongo/src/main/java/org/apache/drill/exec/store/mongo/MongoSubScan.java
+++ contrib/storage-mongo/src/main/java/org/apache/drill/exec/store/mongo/MongoSubScan.java
@@ -1,5 +1,24 @@
 package org.apache.drill.exec.store.mongo;
 
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.drill.exec.store.mongo;
+
 import java.util.Iterator;
 import java.util.List;
 
@@ -9,7 +28,9 @@ import org.apache.drill.exec.physical.base.PhysicalOperator;
 import org.apache.drill.exec.physical.base.PhysicalVisitor;
 import org.apache.drill.exec.physical.base.SubScan;
 
-public class MongoSubScan extends AbstractBase implements SubScan{
+import com.mongodb.MongoClient;
+
+public class MongoSubScan extends AbstractBase implements SubScan {
 
   @Override
   public <T, X, E extends Throwable> T accept(
@@ -31,6 +52,18 @@ public class MongoSubScan extends AbstractBase implements SubScan{
   @Override
   public Iterator<PhysicalOperator> iterator() {
     return null;
+  }
+
+  public static class MongoSubScanSpec {
+    protected MongoClient mongoClient;
+
+    public MongoClient getMongoClient() {
+      return mongoClient;
+    }
+
+    public void setMongoClient(MongoClient mongoClient) {
+      this.mongoClient = mongoClient;
+    }
   }
 
 }
