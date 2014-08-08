f868982cc112e7938fc298ea81bdc9a25aac5c01
Kamesh <kam.iitkgp@gmail.com>
2014-08-08 12:11:16 +0530

    Formating toString method

--- contrib/storage-mongo/src/main/java/org/apache/drill/exec/store/mongo/common/ChunkInfo.java
+++ contrib/storage-mongo/src/main/java/org/apache/drill/exec/store/mongo/common/ChunkInfo.java
@@ -40,7 +40,7 @@ public class ChunkInfo {
 
   @Override
   public String toString() {
-    return "ChunkInfo [ " + "chunkLocList" + chunkLocList + ", chunkId " + chunkId + " ]";
+    return "ChunkInfo [" + "chunkLocList=" + chunkLocList + ", chunkId=" + chunkId + "]";
   }
 
 }
