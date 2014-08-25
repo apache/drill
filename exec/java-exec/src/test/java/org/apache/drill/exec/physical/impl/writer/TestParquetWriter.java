b5bbed84bccdff39f1b13c4d529fa8dbf8652220
Aditya Kishore <aditya@maprtech.com>
2014-08-25 23:48:18 +0530

    Fix test case failures in TestParquetWriter
    * Use different table names for different test cases.

--- exec/java-exec/src/test/java/org/apache/drill/exec/physical/impl/writer/TestParquetWriter.java
+++ exec/java-exec/src/test/java/org/apache/drill/exec/physical/impl/writer/TestParquetWriter.java
@@ -92,7 +92,7 @@ public class TestParquetWriter extends BaseTestQuery {
   @Test
   public void testTPCHReadWrite1() throws Exception {
     String inputTable = "cp.`tpch/lineitem.parquet`";
-    runTestAndValidate("*", "*", inputTable, "lineitem_parquet");
+    runTestAndValidate("*", "*", inputTable, "lineitem_parquet_all");
   }
 
   @Test
@@ -102,7 +102,7 @@ public class TestParquetWriter extends BaseTestQuery {
     String validationSelection = "L_ORDERKEY, L_PARTKEY, L_SUPPKEY, L_LINENUMBER, L_QUANTITY, L_EXTENDEDPRICE, L_DISCOUNT, L_TAX, " +
         "L_RETURNFLAG, L_LINESTATUS, L_SHIPDATE,L_COMMITDATE ,L_RECEIPTDATE, L_SHIPINSTRUCT, L_SHIPMODE, L_COMMENT";
     String inputTable = "cp.`tpch/lineitem.parquet`";
-    runTestAndValidate(selection, validationSelection, inputTable, "lineitem_parquet");
+    runTestAndValidate(selection, validationSelection, inputTable, "lineitem_parquet_converted");
   }
 
   @Test
