807c4766ad7acc9c269da49d84d149097a11d1b0
Hanifi Gunes <hgunes@maprtech.com>
2014-10-22 15:05:22 -0700

    DRILL-1435: ensure producer consumer returns the last batch indicator & prevent blocking/leaking producer & consumer threads; fix concurrency issues that drives sqlline & drillbit hanging

--- exec/java-exec/src/main/java/org/apache/drill/exec/physical/impl/producer/ProducerConsumerBatch.java
+++ exec/java-exec/src/main/java/org/apache/drill/exec/physical/impl/producer/ProducerConsumerBatch.java
@@ -20,6 +20,7 @@ package org.apache.drill.exec.physical.impl.producer;
 import java.util.concurrent.BlockingDeque;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.LinkedBlockingDeque;
+import java.util.concurrent.TimeUnit;
 
 import org.apache.drill.common.types.TypeProtos.MajorType;
 import org.apache.drill.exec.exception.SchemaChangeException;
@@ -153,33 +154,35 @@ public class ProducerConsumerBatch extends AbstractRecordBatch {
           IterOutcome upstream = incoming.next();
           switch (upstream) {
             case NONE:
+              stop = true;
               break outer;
             case STOP:
               queue.putFirst(new RecordBatchDataWrapper(null, false, true));
               return;
             case OK_NEW_SCHEMA:
             case OK:
-              try {
-                if (!stop) {
-                  wrapper = new RecordBatchDataWrapper(new RecordBatchData(incoming), false, false);
-                  queue.put(wrapper);
-                }
-              } catch (InterruptedException e) {
-                wrapper.batch.getContainer().zeroVectors();
-                throw e;
-              }
+              wrapper = new RecordBatchDataWrapper(new RecordBatchData(incoming), false, false);
+              queue.put(wrapper);
+              wrapper = null;
               break;
             default:
               throw new UnsupportedOperationException();
           }
         }
-
-        queue.put(new RecordBatchDataWrapper(null, true, false));
       } catch (InterruptedException e) {
-        if (!(context.isCancelled() || context.isFailed())) {
-          context.fail(e);
-        }
+        logger.warn("Producer thread is interrupted.", e);
       } finally {
+        if (stop) {
+          try {
+            clearQueue();
+            queue.put(new RecordBatchDataWrapper(null, true, false));
+          } catch (InterruptedException e) {
+            logger.error("Unable to enqueue the last batch indicator. Something is broken.", e);
+          }
+        }
+        if (wrapper!=null) {
+          wrapper.batch.getContainer().zeroVectors();
+        }
         cleanUpLatch.countDown();
       }
     }
