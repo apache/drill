1944b434d7f2ac047d5ca188f14a784da37ce8c6
Jason Altekruse <altekrusejason@gmail.com>
2014-11-24 22:30:53 -0800

    DRILL-1185: Unit test for delimited text failing in classpath storage plugin.

--- exec/java-exec/src/test/java/org/apache/drill/TestExampleQueries.java
+++ exec/java-exec/src/test/java/org/apache/drill/TestExampleQueries.java
@@ -36,6 +36,11 @@ public class TestExampleQueries extends BaseTestQuery{
   }
 
   @Test
+  public void testTextInClasspathStorage() throws Exception {
+    test("select * from cp.`/store/text/classpath_storage_csv_test.csv`");
+  }
+
+  @Test
   public void testParquetComplex() throws Exception {
     test("select recipe from cp.`parquet/complex.parquet`");
     test("select * from cp.`parquet/complex.parquet`");
