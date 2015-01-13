hnfgns/incubator-drill

DRILL-1885: fix a problem regarding ordinal to vector mapping that report incorrect result or fails a query & refactor code, eliminiate redundancy support case insensitive vector lookup yet case sensitive result reporting rely on fields in the order that they show up in the schema while copying vectors

exec/java-exec/src/main/java/org/apache/drill/exec/vector/complex/impl/ComplexWriterImpl.java
@@ -190,7 +190,7 @@ public class ComplexWriterImpl extends AbstractFieldWriter implements ComplexWri
     @Override
     public <T extends ValueVector> T addOrGet(String name, MajorType type, Class<T> clazz) {
       ValueVector v = vc.addOrGet(name, type, clazz);
-      this.put(name, v);
+      putVector(name, v);
       return this.typeify(v, clazz);
 
     }

exec/java-exec/src/main/java/org/apache/drill/exec/vector/complex/impl/RepeatedMapReaderImpl.java
@@ -50,7 +50,7 @@ public class RepeatedMapReaderImpl extends AbstractFieldReader{
   public FieldReader reader(String name) {
     FieldReader reader = fields.get(name);
     if (reader == null) {
-      ValueVector child = vector.get(name, ValueVector.class);
+      ValueVector child = vector.getVector(name);
       if (child == null) {
         reader = NullReader.INSTANCE;
       } else {

exec/java-exec/src/main/java/org/apache/drill/exec/vector/complex/impl/SingleMapReaderImpl.java
@@ -51,7 +51,7 @@ public class SingleMapReaderImpl extends AbstractFieldReader{
   public FieldReader reader(String name){
     FieldReader reader = fields.get(name);
     if(reader == null){
-      ValueVector child = vector.get(name, ValueVector.class);
+      ValueVector child = vector.getVector(name);
       if(child == null){
         reader = NullReader.INSTANCE;
       }else{

exec/java-exec/src/main/java/org/apache/drill/exec/vector/complex/impl/VectorContainerWriter.java
@@ -94,7 +94,7 @@ public class VectorContainerWriter extends AbstractFieldWriter implements Comple
     public <T extends ValueVector> T addOrGet(String name, MajorType type, Class<T> clazz) {
       try {
         ValueVector v = mutator.addField(MaterializedField.create(name, type), clazz);
-        this.put(name, v);
+        putVector(name, v);
         return this.typeify(v, clazz);
       } catch (SchemaChangeException e) {
         throw new IllegalStateException(e);

apache/drill

exec/java-exec/src/main/java/org/apache/drill/exec/vector/complex/impl/ComplexWriterImpl.java
@@ -190,7 +190,7 @@ public class ComplexWriterImpl extends AbstractFieldWriter implements ComplexWri
     @Override
     public <T extends ValueVector> T addOrGet(String name, MajorType type, Class<T> clazz) {
       ValueVector v = vc.addOrGet(name, type, clazz);
-      this.put(name, v);
+      putChild(name, v);
       return this.typeify(v, clazz);
 
     }

exec/java-exec/src/main/java/org/apache/drill/exec/vector/complex/impl/RepeatedMapReaderImpl.java
@@ -50,7 +50,7 @@ public class RepeatedMapReaderImpl extends AbstractFieldReader{
   public FieldReader reader(String name) {
     FieldReader reader = fields.get(name);
     if (reader == null) {
-      ValueVector child = vector.get(name, ValueVector.class);
+      ValueVector child = vector.getChild(name);
       if (child == null) {
         reader = NullReader.INSTANCE;
       } else {

exec/java-exec/src/main/java/org/apache/drill/exec/vector/complex/impl/SingleMapReaderImpl.java
@@ -51,7 +51,7 @@ public class SingleMapReaderImpl extends AbstractFieldReader{
   public FieldReader reader(String name){
     FieldReader reader = fields.get(name);
     if(reader == null){
-      ValueVector child = vector.get(name, ValueVector.class);
+      ValueVector child = vector.getChild(name);
       if(child == null){
         reader = NullReader.INSTANCE;
       }else{

exec/java-exec/src/main/java/org/apache/drill/exec/vector/complex/impl/VectorContainerWriter.java
@@ -94,7 +94,7 @@ public class VectorContainerWriter extends AbstractFieldWriter implements Comple
     public <T extends ValueVector> T addOrGet(String name, MajorType type, Class<T> clazz) {
       try {
         ValueVector v = mutator.addField(MaterializedField.create(name, type), clazz);
-        this.put(name, v);
+        putChild(name, v);
         return this.typeify(v, clazz);
       } catch (SchemaChangeException e) {
         throw new IllegalStateException(e);