yufeldman/incubator-drill

DRILL-133: LocalExchange planning and exec.

exec/java-exec/src/main/java/org/apache/drill/exec/server/options/SystemOptionManager.java
@@ -50,6 +50,7 @@ public class SystemOptionManager implements OptionManager {
       PlannerSettings.BROADCAST,
       PlannerSettings.BROADCAST_THRESHOLD,
       PlannerSettings.JOIN_ROW_COUNT_ESTIMATE_FACTOR,
+      PlannerSettings.LOCAL_EXCHANGES,
       PlannerSettings.PRODUCER_CONSUMER,
       PlannerSettings.PRODUCER_CONSUMER_QUEUE_SIZE,
       PlannerSettings.HASH_SINGLE_KEY,

apache/drill

exec/java-exec/src/main/java/org/apache/drill/exec/server/options/SystemOptionManager.java
@@ -51,6 +51,8 @@ public class SystemOptionManager implements OptionManager {
       PlannerSettings.BROADCAST_THRESHOLD,
       PlannerSettings.BROADCAST_FACTOR,
       PlannerSettings.JOIN_ROW_COUNT_ESTIMATE_FACTOR,
+      PlannerSettings.MUX_EXCHANGE,
+      PlannerSettings.DEMUX_EXCHANGE,
       PlannerSettings.PRODUCER_CONSUMER,
       PlannerSettings.PRODUCER_CONSUMER_QUEUE_SIZE,
       PlannerSettings.HASH_SINGLE_KEY,