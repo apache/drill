yufeldman/incubator-drill

DRILL-2210 Introducing multithreading capability to PartitonerSender

exec/java-exec/src/main/java/org/apache/drill/exec/server/options/SystemOptionManager.java
@@ -56,6 +56,7 @@ public class SystemOptionManager implements OptionManager {
       PlannerSettings.PRODUCER_CONSUMER_QUEUE_SIZE,
       PlannerSettings.HASH_SINGLE_KEY,
       PlannerSettings.IDENTIFIER_MAX_LENGTH,
+      PlannerSettings.PARTITION_SENDER_THREADS,
       ExecConstants.CAST_TO_NULLABLE_NUMERIC_OPTION,
       ExecConstants.OUTPUT_FORMAT_VALIDATOR,
       ExecConstants.PARQUET_BLOCK_SIZE_VALIDATOR,

apache/drill

exec/java-exec/src/main/java/org/apache/drill/exec/server/options/SystemOptionManager.java
@@ -59,6 +59,9 @@ public class SystemOptionManager implements OptionManager {
       PlannerSettings.IDENTIFIER_MAX_LENGTH,
       PlannerSettings.HASH_JOIN_SWAP,
       PlannerSettings.HASH_JOIN_SWAP_MARGIN_FACTOR,
+      PlannerSettings.PARTITION_SENDER_THREADS_FACTOR,
+      PlannerSettings.PARTITION_SENDER_MAX_THREADS,
+      PlannerSettings.PARTITION_SENDER_SET_THREADS,
       ExecConstants.CAST_TO_NULLABLE_NUMERIC_OPTION,
       ExecConstants.OUTPUT_FORMAT_VALIDATOR,
       ExecConstants.PARQUET_BLOCK_SIZE_VALIDATOR,