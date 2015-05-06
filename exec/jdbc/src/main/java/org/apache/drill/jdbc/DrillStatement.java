1d3ffe84f3237d614b524702e89be8cce1509931
Kunal Khatua <kkhatua@maprtech.com>
2015-05-06 09:37:56 -0700

    DRILL-2961: Throw SQLException when attempting to set query timeout
    Statement.setQueryTimeout(int seconds) is a No-Op, leading the user to
    believe that a timeout has been set. This provides the correct response
    (SQLException for 'method not supported')

--- exec/jdbc/src/main/java/org/apache/drill/jdbc/DrillStatement.java
+++ exec/jdbc/src/main/java/org/apache/drill/jdbc/DrillStatement.java
@@ -84,4 +84,8 @@ public abstract class DrillStatement extends AvaticaStatement
     connection1.openStatementsRegistry.removeStatement(this);
   }
 
+  @Override
+  public void setQueryTimeout(int seconds) throws SQLException {
+	throw new SQLException("Method is not supported");
+  }
 }
