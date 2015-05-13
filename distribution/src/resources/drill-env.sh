20b36885c12b93af1fcfeda81ac35eada8436903
Steven Phillips <smp@apache.org>
2015-05-12 17:49:12 -0700

    DRILL-3048: Disable assertions by default in distribution

--- distribution/src/resources/drill-env.sh
+++ distribution/src/resources/drill-env.sh
@@ -16,7 +16,7 @@
 DRILL_MAX_DIRECT_MEMORY="8G"
 DRILL_HEAP="4G"
 
-export DRILL_JAVA_OPTS="-Xms$DRILL_HEAP -Xmx$DRILL_HEAP -XX:MaxDirectMemorySize=$DRILL_MAX_DIRECT_MEMORY -XX:MaxPermSize=512M -XX:ReservedCodeCacheSize=1G -Ddrill.exec.enable-epoll=true -ea"
+export DRILL_JAVA_OPTS="-Xms$DRILL_HEAP -Xmx$DRILL_HEAP -XX:MaxDirectMemorySize=$DRILL_MAX_DIRECT_MEMORY -XX:MaxPermSize=512M -XX:ReservedCodeCacheSize=1G -Ddrill.exec.enable-epoll=true"
 
 # Class unloading is disabled by default in Java 7
 # http://hg.openjdk.java.net/jdk7u/jdk7u60/hotspot/file/tip/src/share/vm/runtime/globals.hpp#l1622
