83a9ebd937960883b65122959540e23882931cb2
Aman Sinha <asinha@maprtech.com>
2015-07-25 23:33:18 -0700

    DRILL-2949: Fix CannotPlanException for TPC-DS 1 and 30 (Fix is CALCITE-714) by pushing join condition into subquery when de-correlating.
    Added unit test.

--- exec/java-exec/src/test/java/org/apache/drill/TestCorrelation.java
+++ exec/java-exec/src/test/java/org/apache/drill/TestCorrelation.java
@@ -37,4 +37,21 @@ public class TestCorrelation extends PlanTestBase {
       .run();
   }
 
+  @Test  // DRILL-2949
+  public void testScalarAggAndFilterCorrelatedSubquery() throws Exception {
+    String query = "select count(*) as cnt from cp.`tpch/nation.parquet` n1, "
+      + " cp.`tpch/region.parquet` r1 where n1.n_regionkey = r1.r_regionkey and "
+      + " r1.r_regionkey < 3 and "
+      + " n1.n_nationkey  > (select avg(n2.n_regionkey) * 4 from cp.`tpch/nation.parquet` n2 "
+      + " where n1.n_regionkey = n2.n_nationkey)";
+
+    testBuilder()
+      .sqlQuery(query)
+      .ordered()
+      .baselineColumns("cnt")
+      .baselineValues((long) 11)
+      .build()
+      .run();
+  }
+
 }
