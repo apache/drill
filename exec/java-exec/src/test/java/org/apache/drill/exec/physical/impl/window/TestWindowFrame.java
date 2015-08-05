b7f37bd03887fe6cc025d3744a383bd2e7f984d2
adeneche <adeneche@gmail.com>
2015-08-05 09:27:24 -0700

    added unit tests for fixed bugs 3604, 3605 and 3606

--- exec/java-exec/src/test/java/org/apache/drill/exec/physical/impl/window/TestWindowFrame.java
+++ exec/java-exec/src/test/java/org/apache/drill/exec/physical/impl/window/TestWindowFrame.java
@@ -146,6 +146,34 @@ public class TestWindowFrame extends BaseTestQuery {
     test(getFile("window/q3220.sql"), TEST_RES_PATH);
   }
 
+  @Test // DRILL-3604
+  public void testFix3604() throws Exception {
+    // make sure the query doesn't fail
+    test(getFile("window/3604.sql"), TEST_RES_PATH);
+  }
+
+  @Test // DRILL-3605
+  public void testFix3605() throws Exception {
+    testBuilder()
+      .sqlQuery(getFile("window/3605.sql"), TEST_RES_PATH)
+      .ordered()
+      .csvBaselineFile("window/3605.tsv")
+      .baselineColumns("col2", "lead_col2")
+      .build()
+      .run();
+  }
+
+  @Test // DRILL-3606
+  public void testFix3606() throws Exception {
+    testBuilder()
+      .sqlQuery(getFile("window/3606.sql"), TEST_RES_PATH)
+      .ordered()
+      .csvBaselineFile("window/3606.tsv")
+      .baselineColumns("col2", "lead_col2")
+      .build()
+      .run();
+  }
+
   @Test
   public void testLead() throws Exception {
     testBuilder()
