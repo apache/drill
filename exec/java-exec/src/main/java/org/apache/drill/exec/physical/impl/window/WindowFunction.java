7e07ec167cf63bb9a34564675ec2982bfbbde9b0
adeneche <adeneche@gmail.com>
2015-08-17 15:15:11 -0700

    DRILL-3643: NTILE(0) returns RuntimeException

--- exec/java-exec/src/main/java/org/apache/drill/exec/physical/impl/window/WindowFunction.java
+++ exec/java-exec/src/main/java/org/apache/drill/exec/physical/impl/window/WindowFunction.java
@@ -21,6 +21,7 @@ import com.sun.codemodel.JExpr;
 import com.sun.codemodel.JExpression;
 import com.sun.codemodel.JInvocation;
 import com.sun.codemodel.JVar;
+import org.apache.drill.common.exceptions.UserException;
 import org.apache.drill.common.expression.FunctionCall;
 import org.apache.drill.common.expression.LogicalExpression;
 import org.apache.drill.common.expression.ValueExpressions;
@@ -40,6 +41,8 @@ import org.apache.drill.exec.record.TypedFieldId;
 import org.apache.drill.exec.record.VectorContainer;
 
 public abstract class WindowFunction {
+  private static final org.slf4j.Logger logger = org.slf4j.LoggerFactory.getLogger(WindowFunction.class);
+
   public enum Type {
     ROW_NUMBER,
     RANK,
@@ -181,12 +184,12 @@ public abstract class WindowFunction {
     private int numTilesFromExpression(LogicalExpression numTilesExpr) {
       if ((numTilesExpr instanceof ValueExpressions.IntExpression)) {
         int nt = ((ValueExpressions.IntExpression) numTilesExpr).getInt();
-        if (nt >= 0) {
+        if (nt > 0) {
           return nt;
         }
       }
 
-      throw new IllegalArgumentException("NTILE only accepts unsigned integer argument");
+      throw UserException.functionError().message("NTILE only accepts positive integer argument").build(logger);
     }
 
     @Override
