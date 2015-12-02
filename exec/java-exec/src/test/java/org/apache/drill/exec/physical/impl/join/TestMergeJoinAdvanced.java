3ae3bf5e127b4384c7b91d797d36ea4d51a058ae
Amit Hadke <amit.hadke@gmail.com>
2015-12-02 14:16:39 -0800

    DRILL-4109 Increase timeout on merge join tests.

--- exec/java-exec/src/test/java/org/apache/drill/exec/physical/impl/join/TestMergeJoinAdvanced.java
+++ exec/java-exec/src/test/java/org/apache/drill/exec/physical/impl/join/TestMergeJoinAdvanced.java
@@ -24,7 +24,9 @@ import org.apache.drill.exec.planner.physical.PlannerSettings;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Ignore;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.TestRule;
 
 import java.io.BufferedWriter;
 import java.io.File;
@@ -33,6 +35,8 @@ import java.io.IOException;
 import java.util.Random;
 
 public class TestMergeJoinAdvanced extends BaseTestQuery {
+  @Rule
+  public final TestRule TIMEOUT = TestTools.getTimeoutRule(120000); // Longer timeout than usual.
 
   // Have to disable hash join to test merge join in this class
   @BeforeClass
