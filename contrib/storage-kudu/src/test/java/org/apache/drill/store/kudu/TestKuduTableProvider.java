805e0416106139e6686672aed2d2cd600bc4e36d
Jason Altekruse <altekrusejason@gmail.com>
2016-01-10 22:50:05 -0800

    DRILL-4241: Fixing the build, make RAT and checkstyle happy.

--- contrib/storage-kudu/src/test/java/org/apache/drill/store/kudu/TestKuduTableProvider.java
+++ contrib/storage-kudu/src/test/java/org/apache/drill/store/kudu/TestKuduTableProvider.java
@@ -17,75 +17,7 @@
  */
 package org.apache.drill.store.kudu;
 
-import static org.junit.Assert.assertEquals;
+import org.apache.drill.BaseTestQuery;
+public class TestKuduTableProvider extends BaseTestQuery {
 
-import java.io.IOException;
-import java.util.Map.Entry;
-
-import org.apache.drill.common.config.LogicalPlanPersistence;
-import org.apache.drill.exec.planner.PhysicalPlanReaderTestFactory;
-import org.apache.drill.exec.store.kudu.config.KuduPStoreProvider;
-import org.apache.drill.exec.store.sys.PStore;
-import org.apache.drill.exec.store.sys.PStoreConfig;
-import org.junit.AfterClass;
-import org.junit.BeforeClass;
-import org.junit.Test;
-
-public class TestKuduTableProvider extends BaseKuduTest {
-
-  private static KuduPStoreProvider provider;
-
-  @BeforeClass // mask Kudu cluster start function
-  public static void setUpBeforeTestKuduTableProvider() throws Exception {
-    provider = new KuduPStoreProvider(storagePluginConfig.getKuduConf(), "drill_store");
-    provider.start();
-  }
-
-  @Test
-  public void testTableProvider() throws IOException {
-    LogicalPlanPersistence lp = PhysicalPlanReaderTestFactory.defaultLogicalPlanPersistence(config);
-    PStore<String> kuduStore = provider.getStore(PStoreConfig.newJacksonBuilder(lp.getMapper(), String.class).name("kudu").build());
-    kuduStore.put("", "v0");
-    kuduStore.put("k1", "v1");
-    kuduStore.put("k2", "v2");
-    kuduStore.put("k3", "v3");
-    kuduStore.put("k4", "v4");
-    kuduStore.put("k5", "v5");
-    kuduStore.put(".test", "testValue");
-
-    assertEquals("v0", kuduStore.get(""));
-    assertEquals("testValue", kuduStore.get(".test"));
-
-    int rowCount = 0;
-    for (Entry<String, String> entry : kuduStore) {
-      rowCount++;
-      System.out.println(entry.getKey() + "=" + entry.getValue());
-    }
-    assertEquals(7, rowCount);
-
-    PStore<String> kuduTestStore = provider.getStore(PStoreConfig.newJacksonBuilder(lp.getMapper(), String.class).name("kudu.test").build());
-    kuduTestStore.put("", "v0");
-    kuduTestStore.put("k1", "v1");
-    kuduTestStore.put("k2", "v2");
-    kuduTestStore.put("k3", "v3");
-    kuduTestStore.put("k4", "v4");
-    kuduTestStore.put(".test", "testValue");
-
-    assertEquals("v0", kuduStore.get(""));
-    assertEquals("testValue", kuduStore.get(".test"));
-
-    rowCount = 0;
-    for (Entry<String, String> entry : kuduTestStore) {
-      rowCount++;
-      System.out.println(entry.getKey() + "=" + entry.getValue());
-    }
-    assertEquals(6, rowCount);
-  }
-
-  @AfterClass
-  public static void tearDownTestKuduTableProvider() {
-    if (provider != null) {
-      provider.close();
-    }
-  }
 }
