161fc252ac1106a934378061002e2f47064c3bab
jean-claude cote <jccote@gmail.com>
2016-05-14 18:02:57 -0400

    DRILL-4573 Fixed issue with regexp_replace function

--- exec/java-exec/src/main/java/org/apache/drill/exec/expr/fn/impl/CharSequenceWrapper.java
+++ exec/java-exec/src/main/java/org/apache/drill/exec/expr/fn/impl/CharSequenceWrapper.java
@@ -17,13 +17,30 @@
  */
 package org.apache.drill.exec.expr.fn.impl;
 
+import java.util.regex.Matcher;
+
 import io.netty.buffer.DrillBuf;
 
+/**
+ * A CharSequence is a readable sequence of char values. This interface provides
+ * uniform, read-only access to many different kinds of char sequences. A char
+ * value represents a character in the Basic Multilingual Plane (BMP) or a
+ * surrogate. Refer to Unicode Character Representation for details.<br>
+ * Specifically this implementation of the CharSequence adapts a Drill
+ * {@link DrillBuf} to the CharSequence. The implementation is meant to be
+ * re-used that is allocated once and then passed DrillBuf to adapt. This can be
+ * handy to exploit API that consume CharSequence avoiding the need to create
+ * string objects.
+ *
+ */
 public class CharSequenceWrapper implements CharSequence {
 
-    private int start;
-    private int end;
+    // The adapted drill buffer
     private DrillBuf buffer;
+    // The start offset into the drill buffer
+    private int start;
+    // The end offset into the drill buffer
+    private int end;
 
     @Override
     public int length() {
@@ -32,18 +49,54 @@ public class CharSequenceWrapper implements CharSequence {
 
     @Override
     public char charAt(int index) {
-        return (char) buffer.getByte(start + index);
+        int charPos = start + index;
+        if(charPos < start || charPos >= end)
+        {
+            throw new IndexOutOfBoundsException();
+        }
+        // Get the char within the DrillBuf.
+        return (char) buffer.getByte(charPos);
     }
 
+    /**
+     * When using the Java regex {@link Matcher} the subSequence is only called
+     * when capturing groups. Drill does not currently use capture groups in the
+     * UDF so this method is not required.<br>
+     * It could be implemented by creating a new CharSequenceWrapper however
+     * this would imply newly allocated objects which is what this wrapper tries
+     * to avoid.
+     *
+     */
     @Override
     public CharSequence subSequence(int start, int end) {
         throw new UnsupportedOperationException("Not implemented.");
     }
 
+    /**
+     * Set the DrillBuf to adapt to a CharSequence. This method can be used to
+     * replace any previous DrillBuf thus avoiding recreating the
+     * CharSequenceWrapper and thus re-using the CharSequenceWrapper object.
+     *
+     * @param start
+     * @param end
+     * @param buffer
+     */
     public void setBuffer(int start, int end, DrillBuf buffer) {
         this.start = start;
         this.end = end;
         this.buffer = buffer;
+    }
+
+    /**
+     * In cases where a method like {@link Matcher#replaceAll(String)} is used this
+     * {@link CharSequence#toString()} method will be called, so we need to return a
+     * string representing the value of this CharSequence. Note however that the
+     * regexp_replace function is implemented in a way to avoid the call to toString()
+     * not to uselessly create a string object.
+     */
+    @Override
+    public String toString() {
+        return StringFunctionHelpers.toStringFromUTF8(start, end, buffer);
     }
 
 }
