810198b18bfbe38712256c58b102bca079d934c1
Serhii-Harnyk <serhii.harnyk@gmail.com>
2016-12-08 20:08:34 +0000

    DRILL-5117: Compile error when query a json file with 1000+columns
    close apache/drill#686

--- exec/java-exec/src/test/java/org/apache/drill/exec/compile/TestLargeFileCompilation.java
+++ exec/java-exec/src/test/java/org/apache/drill/exec/compile/TestLargeFileCompilation.java
@@ -20,7 +20,6 @@ package org.apache.drill.exec.compile;
 import org.apache.drill.BaseTestQuery;
 import org.apache.drill.common.util.TestTools;
 import org.apache.drill.exec.ExecConstants;
-import org.junit.Ignore;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.TestRule;
@@ -129,14 +128,12 @@ public class TestLargeFileCompilation extends BaseTestQuery {
   }
 
   @Test
-  @Ignore("DRILL-1808")
   public void testEXTERNAL_SORT() throws Exception {
     testNoResult("alter session set `%s`='JDK'", QueryClassLoader.JAVA_COMPILER_OPTION);
     testNoResult(ITERATION_COUNT, LARGE_QUERY_ORDER_BY);
   }
 
   @Test
-  @Ignore("DRILL-1808")
   public void testTOP_N_SORT() throws Exception {
     testNoResult("alter session set `%s`='JDK'", QueryClassLoader.JAVA_COMPILER_OPTION);
     testNoResult(ITERATION_COUNT, LARGE_QUERY_ORDER_BY_WITH_LIMIT);
@@ -153,5 +150,4 @@ public class TestLargeFileCompilation extends BaseTestQuery {
     testNoResult("alter session set `%s`='JDK'", QueryClassLoader.JAVA_COMPILER_OPTION);
     testNoResult(ITERATION_COUNT, LARGE_QUERY_SELECT_LIST);
   }
-
 }
