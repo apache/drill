c9126f918c8ba4bf5f19b1dd27a3944b31b9b628
Sorabh Hamirwasia <shamirwasia@maprtech.com>
2016-12-12 11:55:47 -0800

    DRILL-5098: HYGIENE: Refactoring BasicClient::close to call RemoteConnection::close

--- exec/rpc/src/main/java/org/apache/drill/exec/rpc/BasicClient.java
+++ exec/rpc/src/main/java/org/apache/drill/exec/rpc/BasicClient.java
@@ -150,9 +150,7 @@ public abstract class BasicClient<T extends EnumLite, R extends RemoteConnection
   public abstract ProtobufLengthDecoder getDecoder(BufferAllocator allocator);
 
   public boolean isActive() {
-    return connection != null
-        && connection.getChannel() != null
-        && connection.getChannel().isActive();
+    return (connection != null) && connection.isActive();
   }
 
   protected abstract void validateHandshake(HANDSHAKE_RESPONSE validateHandshake) throws RpcException;
@@ -302,14 +300,9 @@ public abstract class BasicClient<T extends EnumLite, R extends RemoteConnection
 
   public void close() {
     logger.debug("Closing client");
-    try {
-      connection.getChannel().close().get();
-    } catch (final InterruptedException | ExecutionException e) {
-      logger.warn("Failure while shutting {}", this.getClass().getName(), e);
 
-      // Preserve evidence that the interruption occurred so that code higher up on the call stack can learn of the
-      // interruption and respond to it if it wants to.
-      Thread.currentThread().interrupt();
+    if (connection != null) {
+      connection.close();
     }
   }
 
