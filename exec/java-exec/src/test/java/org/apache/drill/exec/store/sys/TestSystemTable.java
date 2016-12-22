6d85db7872b993d4e750ae76eba39d8949b82e4c
hongze.zhz <hongze.zhz@alibaba-inc.com>
2016-12-22 15:47:42 +0800

    DRILL-5068: Add a new system table for completed profiles

--- exec/java-exec/src/test/java/org/apache/drill/exec/store/sys/TestSystemTable.java
+++ exec/java-exec/src/test/java/org/apache/drill/exec/store/sys/TestSystemTable.java
@@ -69,4 +69,9 @@ public class TestSystemTable extends BaseTestQuery {
   public void memoryTable() throws Exception {
     test("select * from sys.memory");
   }
+
+  @Test
+  public void profilesTable() throws Exception {
+    test("select * from sys.profiles");
+  }
 }
