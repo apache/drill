74edbd9d7052124c77ae8c580a88979e9544f4b2
Timothy Farkas <timothyfarkas@apache.org>
2017-09-16 01:28:49 -0700

    - More fixes

--- common/src/test/java/org/apache/drill/test/DrillTest.java
+++ common/src/test/java/org/apache/drill/test/DrillTest.java
@@ -57,7 +57,7 @@ public class DrillTest {
   static MemWatcher memWatcher;
   static String className;
 
-  @Rule public final TestRule TIMEOUT = TestTools.getTimeoutRule(50000);
+  @Rule public final TestRule TIMEOUT = TestTools.getTimeoutRule(1000000);
   @Rule public final TestLogReporter logOutcome = LOG_OUTCOME;
 
   @Rule public final TestRule REPEAT_RULE = TestTools.getRepeatRule(false);
