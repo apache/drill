416bc527c4528d93ea23169292bd42578a4c647a
Paul Rogers <progers@maprtech.com>
2017-10-12 15:27:31 -0700

    DRILL-5872: Workaround for invalid cost in physical plans
    See DRILL-5872 for details.

--- exec/java-exec/src/main/java/org/apache/drill/exec/work/foreman/Foreman.java
+++ exec/java-exec/src/main/java/org/apache/drill/exec/work/foreman/Foreman.java
@@ -419,8 +419,9 @@ public class Foreman implements Runnable {
     queryRM.visitAbstractPlan(plan);
     final QueryWorkUnit work = getQueryWorkUnit(plan);
     queryRM.visitPhysicalPlan(work);
-    queryRM.setCost(plan.totalCost());
-    queryManager.setTotalCost(plan.totalCost());
+    final double totalCost = plan.totalCost();
+    queryRM.setCost(totalCost);
+    queryManager.setTotalCost(totalCost);
     work.applyPlan(drillbitContext.getPlanReader());
     logWorkUnit(work);
     admit(work);
