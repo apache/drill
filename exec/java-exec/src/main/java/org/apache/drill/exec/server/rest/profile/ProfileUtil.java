16e525ea536e3af8110b2a902a417b70fa8b49a4
Prasad Nagaraj Subramanya <prasadns14@gmail.com>
2017-11-09 15:00:15 -0800

    DRILL-5923: Display name for query state

---
+++ exec/java-exec/src/main/java/org/apache/drill/exec/server/rest/profile/ProfileUtil.java
@@ -0,0 +1,48 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.drill.exec.server.rest.profile;
+
+import org.apache.drill.exec.proto.UserBitShared.QueryResult.QueryState;
+
+public class ProfileUtil {
+  // Display names for QueryState enum in UserBitShared.proto
+  private static final String[] queryStateDisplayNames = {
+    "Starting", // STARTING = 0
+    "Running", // RUNNING = 1
+    "Succeeded", // COMPLETED = 2
+    "Canceled", // CANCELED = 3
+    "Failed", // FAILED = 4
+    "CancellationRequested", // CANCELLATION_REQUESTED = 5
+    "Enqueued" // ENQUEUED = 6
+  };
+
+
+  /**
+   * Utility to return display name for query state
+   * @param queryState
+   * @return display string for query state
+   */
+  public final static String getQueryStateDisplayName(QueryState queryState) {
+    int queryStateOrdinal = queryState.getNumber();
+    if (queryStateOrdinal >= queryStateDisplayNames.length) {
+      return queryState.name();
+    } else {
+      return queryStateDisplayNames[queryStateOrdinal];
+    }
+  }
+}
