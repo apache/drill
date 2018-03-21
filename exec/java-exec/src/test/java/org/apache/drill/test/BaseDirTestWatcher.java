7f645565cd68b1780e643bc20a43951610704008
Timothy Farkas <timothyfarkas@apache.org>
2018-03-20 23:00:22 -0700

    DRILL-6278: Removed temp codegen directory in testing framework.
    close apache/drill#1178

--- exec/java-exec/src/test/java/org/apache/drill/test/BaseDirTestWatcher.java
+++ exec/java-exec/src/test/java/org/apache/drill/test/BaseDirTestWatcher.java
@@ -53,7 +53,6 @@ public class BaseDirTestWatcher extends DirTestWatcher {
     TEST_TMP // Corresponds to the directory that should be mapped to dfs.tmp
   }
 
-  private File codegenDir;
   private File tmpDir;
   private File storeDir;
   private File dfsTestTmpParentDir;
@@ -79,7 +78,6 @@ public class BaseDirTestWatcher extends DirTestWatcher {
   protected void starting(Description description) {
     super.starting(description);
 
-    codegenDir = makeSubDir(Paths.get("codegen"));
     rootDir = makeSubDir(Paths.get("root"));
     tmpDir = makeSubDir(Paths.get("tmp"));
     storeDir = makeSubDir(Paths.get("store"));
@@ -132,14 +130,6 @@ public class BaseDirTestWatcher extends DirTestWatcher {
    */
   public File getRootDir() {
     return rootDir;
-  }
-
-  /**
-   * Gets the temp directory that should be used to save generated code files.
-   * @return The temp directory that should be used to save generated code files.
-   */
-  public File getCodegenDir() {
-    return codegenDir;
   }
 
   /**
