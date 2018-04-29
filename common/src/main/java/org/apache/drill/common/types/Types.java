8a7b577ff575b8ab315d89ef219e67e4cd5b1723
Paul Rogers <progers@cloudera.com>
2018-04-28 23:04:26 -0700

    DRILL-6361: Revised typeOf() function versions

--- common/src/main/java/org/apache/drill/common/types/Types.java
+++ common/src/main/java/org/apache/drill/common/types/Types.java
@@ -28,9 +28,9 @@ import org.apache.drill.common.exceptions.DrillRuntimeException;
 import org.apache.drill.common.types.TypeProtos.DataMode;
 import org.apache.drill.common.types.TypeProtos.MajorType;
 import org.apache.drill.common.types.TypeProtos.MinorType;
+import org.apache.drill.common.util.CoreDecimalUtility;
 
 import com.google.protobuf.TextFormat;
-import org.apache.drill.common.util.CoreDecimalUtility;
 
 public class Types {
   static final org.slf4j.Logger logger = org.slf4j.LoggerFactory.getLogger(Types.class);
@@ -107,6 +107,10 @@ public class Types {
     if (type.getMode() == DataMode.REPEATED || type.getMinorType() == MinorType.LIST) {
       return "ARRAY";
     }
+    return getBaseSqlTypeName(type);
+  }
+
+  public static String getBaseSqlTypeName(final MajorType type) {
 
     switch (type.getMinorType()) {
 
@@ -172,6 +176,19 @@ public class Types {
       default:
         throw new AssertionError(
             "Unexpected/unhandled MinorType value " + type.getMinorType() );
+    }
+  }
+
+  public static String getSqlModeName(final MajorType type) {
+    switch (type.getMode()) {
+    case REQUIRED:
+      return "NOT NULL";
+    case OPTIONAL:
+      return "NULLABLE";
+    case REPEATED:
+      return "ARRAY";
+    default:
+      return "UNKNOWN";
     }
   }
 
