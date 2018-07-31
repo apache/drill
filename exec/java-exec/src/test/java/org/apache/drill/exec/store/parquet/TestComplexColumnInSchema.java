191f160b655fcb060c76e19e3ff5a9794533151e
Ben-Zvi <bben-zvi@mapr.com>
2018-07-31 15:48:06 -0700

    DRILL-6651: Add missing package statements

--- exec/java-exec/src/test/java/org/apache/drill/exec/store/parquet/TestComplexColumnInSchema.java
+++ exec/java-exec/src/test/java/org/apache/drill/exec/store/parquet/TestComplexColumnInSchema.java
@@ -15,9 +15,9 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-import org.apache.drill.common.expression.SchemaPath;
-import org.apache.drill.exec.store.parquet.ParquetReaderUtility;
+package org.apache.drill.exec.store.parquet;
 
+import org.apache.drill.common.expression.SchemaPath;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
 import org.apache.parquet.hadoop.metadata.ParquetMetadata;
