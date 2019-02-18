8f42c3fc566c5a6bdcc92c706114a152b0721300
Kunal Khatua <kunal@apache.org>
2019-02-18 14:02:12 -0800

    RPCType protobuf Update

--- protocol/src/main/java/org/apache/drill/exec/proto/beans/RpcType.java
+++ protocol/src/main/java/org/apache/drill/exec/proto/beans/RpcType.java
@@ -25,9 +25,28 @@ public enum RpcType implements com.dyuproject.protostuff.EnumLite<RpcType>
     HANDSHAKE(0),
     ACK(1),
     GOODBYE(2),
-    REQ_RECORD_BATCH(3),
-    SASL_MESSAGE(4),
-    REQ_RUNTIME_FILTER(5);
+    RUN_QUERY(3),
+    CANCEL_QUERY(4),
+    REQUEST_RESULTS(5),
+    RESUME_PAUSED_QUERY(11),
+    GET_QUERY_PLAN_FRAGMENTS(12),
+    GET_CATALOGS(14),
+    GET_SCHEMAS(15),
+    GET_TABLES(16),
+    GET_COLUMNS(17),
+    CREATE_PREPARED_STATEMENT(22),
+    GET_SERVER_META(8),
+    QUERY_DATA(6),
+    QUERY_HANDLE(7),
+    QUERY_PLAN_FRAGMENTS(13),
+    CATALOGS(18),
+    SCHEMAS(19),
+    TABLES(20),
+    COLUMNS(21),
+    PREPARED_STATEMENT(23),
+    SERVER_META(9),
+    QUERY_RESULT(10),
+    SASL_MESSAGE(24);
     
     public final int number;
     
@@ -48,9 +67,28 @@ public enum RpcType implements com.dyuproject.protostuff.EnumLite<RpcType>
             case 0: return HANDSHAKE;
             case 1: return ACK;
             case 2: return GOODBYE;
-            case 3: return REQ_RECORD_BATCH;
-            case 4: return SASL_MESSAGE;
-            case 5: return REQ_RUNTIME_FILTER;
+            case 3: return RUN_QUERY;
+            case 4: return CANCEL_QUERY;
+            case 5: return REQUEST_RESULTS;
+            case 6: return QUERY_DATA;
+            case 7: return QUERY_HANDLE;
+            case 8: return GET_SERVER_META;
+            case 9: return SERVER_META;
+            case 10: return QUERY_RESULT;
+            case 11: return RESUME_PAUSED_QUERY;
+            case 12: return GET_QUERY_PLAN_FRAGMENTS;
+            case 13: return QUERY_PLAN_FRAGMENTS;
+            case 14: return GET_CATALOGS;
+            case 15: return GET_SCHEMAS;
+            case 16: return GET_TABLES;
+            case 17: return GET_COLUMNS;
+            case 18: return CATALOGS;
+            case 19: return SCHEMAS;
+            case 20: return TABLES;
+            case 21: return COLUMNS;
+            case 22: return CREATE_PREPARED_STATEMENT;
+            case 23: return PREPARED_STATEMENT;
+            case 24: return SASL_MESSAGE;
             default: return null;
         }
     }
