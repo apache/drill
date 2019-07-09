22947ad336d8dd0d944a66ded16a7ce9ffa62f04
Anton Gozhiy <anton5813@gmail.com>
2019-07-09 18:04:52 +0300

    DRILL-7084: ResultSet getObject method throws not implemented exception if the column type is NULL
    closes #1825

--- exec/jdbc/src/test/java/org/apache/drill/jdbc/DrillResultSetTest.java
+++ exec/jdbc/src/test/java/org/apache/drill/jdbc/DrillResultSetTest.java
@@ -18,11 +18,10 @@
 package org.apache.drill.jdbc;
 
 import static org.hamcrest.CoreMatchers.equalTo;
-import static org.hamcrest.CoreMatchers.instanceOf;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.core.StringContains.containsString;
+import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertThat;
-import static org.junit.Assert.fail;
 
 import java.sql.Connection;
 import java.sql.ResultSet;
@@ -34,8 +33,10 @@ import org.apache.drill.exec.ExecConstants;
 import org.apache.drill.categories.JdbcTest;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.experimental.categories.Category;
+import org.junit.rules.ExpectedException;
 
 @Category({SlowTest.class, JdbcTest.class})
 public class DrillResultSetTest extends JdbcTestBase {
@@ -45,123 +46,119 @@ public class DrillResultSetTest extends JdbcTestBase {
       ExecConstants.HTTP_ENABLE;
 
   private static final String origStatusServerPropValue =
-      System.getProperty( STATUS_SERVER_PROPERTY_NAME, "true" );
+      System.getProperty(STATUS_SERVER_PROPERTY_NAME, "true");
+
+  @Rule
+  public ExpectedException thrown = ExpectedException.none();
 
   // Disable Jetty status server so unit tests run (outside Maven setup).
   // (TODO:  Move this to base test class and/or have Jetty try other ports.)
   @BeforeClass
   public static void setUpClass() {
-    System.setProperty( STATUS_SERVER_PROPERTY_NAME, "false" );
+    System.setProperty(STATUS_SERVER_PROPERTY_NAME, "false");
   }
 
   @AfterClass
   public static void tearDownClass() {
-    System.setProperty( STATUS_SERVER_PROPERTY_NAME, origStatusServerPropValue );
+    System.setProperty(STATUS_SERVER_PROPERTY_NAME, origStatusServerPropValue);
   }
 
 
   @Test
   public void test_next_blocksFurtherAccessAfterEnd()
-      throws SQLException
-  {
+      throws SQLException {
     Connection connection = connect();
     Statement statement = connection.createStatement();
     ResultSet resultSet =
-        statement.executeQuery( "SELECT 1 AS x \n" +
-                                "FROM cp.`donuts.json` \n" +
-                                "LIMIT 2" );
+        statement.executeQuery("SELECT 1 AS x \n" +
+            "FROM cp.`donuts.json` \n" +
+            "LIMIT 2");
 
     // Advance to first row; confirm can access data.
-    assertThat( resultSet.next(), is( true ) );
-    assertThat( resultSet.getInt( 1 ), is ( 1 ) );
+    assertThat(resultSet.next(), is(true));
+    assertThat(resultSet.getInt(1), is(1));
 
     // Advance from first to second (last) row, confirming data access.
-    assertThat( resultSet.next(), is( true ) );
-    assertThat( resultSet.getInt( 1 ), is ( 1 ) );
+    assertThat(resultSet.next(), is(true));
+    assertThat(resultSet.getInt(1), is(1));
 
     // Now advance past last row.
-    assertThat( resultSet.next(), is( false ) );
+    assertThat(resultSet.next(), is(false));
 
     // Main check:  That row data access methods now throw SQLException.
-    try {
-      resultSet.getInt( 1 );
-      fail( "Didn't get expected SQLException." );
-    }
-    catch ( SQLException e ) {
-      // Expect something like current InvalidCursorStateSqlException saying
-      // "Result set cursor is already positioned past all rows."
-      assertThat( e, instanceOf( InvalidCursorStateSqlException.class ) );
-      assertThat( e.toString(), containsString( "past" ) );
-    }
-    // (Any other exception is unexpected result.)
+    thrown.expect(InvalidCursorStateSqlException.class);
+    thrown.expectMessage(containsString("Result set cursor is already positioned past all rows."));
+    resultSet.getInt(1);
 
-    assertThat( resultSet.next(), is( false ) );
+    assertThat(resultSet.next(), is(false));
 
     // TODO:  Ideally, test all other accessor methods.
   }
 
   @Test
   public void test_next_blocksFurtherAccessWhenNoRows()
-    throws Exception
-  {
+      throws Exception {
     Connection connection = connect();
     Statement statement = connection.createStatement();
     ResultSet resultSet =
-        statement.executeQuery( "SELECT 'Hi' AS x \n" +
-                                "FROM cp.`donuts.json` \n" +
-                                "WHERE false" );
+        statement.executeQuery("SELECT 'Hi' AS x \n" +
+            "FROM cp.`donuts.json` \n" +
+            "WHERE false");
 
     // Do initial next(). (Advance from before results to next possible
     // position (after the set of zero rows).
-    assertThat( resultSet.next(), is( false ) );
+    assertThat(resultSet.next(), is(false));
 
     // Main check:  That row data access methods throw SQLException.
-    try {
-      resultSet.getString( 1 );
-      fail( "Didn't get expected SQLException." );
-    }
-    catch ( SQLException e ) {
-      // Expect something like current InvalidRowSQLException saying
-      // "Result set cursor is already positioned past all rows."
-      assertThat( e, instanceOf( InvalidCursorStateSqlException.class ) );
-      assertThat( e.toString(), containsString( "past" ) );
-      assertThat( e.toString(), containsString( "rows" ) );
-    }
-    // (Any non-SQLException exception is unexpected result.)
+    thrown.expect(InvalidCursorStateSqlException.class);
+    thrown.expectMessage(containsString("Result set cursor is already positioned past all rows."));
+    resultSet.getString(1);
 
-    assertThat( resultSet.next(), is( false ) );
+    assertThat(resultSet.next(), is(false));
 
     // TODO:  Ideally, test all other accessor methods.
   }
 
   @Test
   public void test_getRow_isOneBased()
-    throws Exception
-  {
+      throws Exception {
     Connection connection = connect();
     Statement statement = connection.createStatement();
     ResultSet resultSet =
-        statement.executeQuery( "VALUES (1), (2)" );
+        statement.executeQuery("VALUES (1), (2)");
 
     // Expect 0 when before first row:
-    assertThat( "getRow() before first next()", resultSet.getRow(), equalTo( 0 ) );
+    assertThat("getRow() before first next()", resultSet.getRow(), equalTo(0));
 
     resultSet.next();
 
     // Expect 1 at first row:
-    assertThat( "getRow() at first row", resultSet.getRow(), equalTo( 1 ) );
+    assertThat("getRow() at first row", resultSet.getRow(), equalTo(1));
 
     resultSet.next();
 
     // Expect 2 at second row:
-    assertThat( "getRow() at second row", resultSet.getRow(), equalTo( 2 ) );
+    assertThat("getRow() at second row", resultSet.getRow(), equalTo(2));
 
     resultSet.next();
 
     // Expect 0 again when after last row:
-    assertThat( "getRow() after last row", resultSet.getRow(), equalTo( 0 ) );
+    assertThat("getRow() after last row", resultSet.getRow(), equalTo(0));
     resultSet.next();
-    assertThat( "getRow() after last row", resultSet.getRow(), equalTo( 0 ) );
+    assertThat("getRow() after last row", resultSet.getRow(), equalTo(0));
+  }
+
+  @Test
+  public void testGetObjectNull() throws SQLException {
+    Connection connection = connect();
+    Statement statement = connection.createStatement();
+    ResultSet resultSet =
+        statement.executeQuery(
+            "select coalesce(a1, b1) " +
+            "from cp.`testGetObjectNull.parquet` " +
+            "limit 1");
+    resultSet.next();
+    assertNull(resultSet.getObject(1));
   }
 
   // TODO:  Ideally, test other methods.
