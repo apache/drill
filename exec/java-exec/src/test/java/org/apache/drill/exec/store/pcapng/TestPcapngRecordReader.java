cfb56f1c5d9c4db0b00ffd88e3831b1e59a67531
luoc <luoc0416@163.com>
2020-12-23 17:11:34 +0800

    DRILL-7533: Convert Pcapng to EVF

--- exec/java-exec/src/test/java/org/apache/drill/exec/store/pcapng/TestPcapngRecordReader.java
+++ exec/java-exec/src/test/java/org/apache/drill/exec/store/pcapng/TestPcapngRecordReader.java
@@ -17,84 +17,201 @@
  */
 package org.apache.drill.exec.store.pcapng;
 
-import org.apache.drill.PlanTestBase;
-import org.apache.drill.common.exceptions.UserRemoteException;
-import org.junit.Assert;
-import org.junit.BeforeClass;
-import org.junit.Test;
+import static org.junit.Assert.assertEquals;
 
 import java.nio.file.Paths;
 
-public class TestPcapngRecordReader extends PlanTestBase {
+import org.apache.drill.categories.RowSetTests;
+import org.apache.drill.common.exceptions.UserRemoteException;
+import org.apache.drill.common.types.TypeProtos.MinorType;
+import org.apache.drill.exec.physical.rowSet.RowSet;
+import org.apache.drill.exec.physical.rowSet.RowSetBuilder;
+import org.apache.drill.exec.record.metadata.SchemaBuilder;
+import org.apache.drill.exec.record.metadata.TupleMetadata;
+import org.apache.drill.test.ClusterFixture;
+import org.apache.drill.test.ClusterTest;
+import org.apache.drill.test.QueryBuilder;
+import org.apache.drill.test.QueryTestUtil;
+import org.apache.drill.test.rowSet.RowSetComparison;
+import org.joda.time.Instant;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.junit.experimental.categories.Category;
+
+@Category(RowSetTests.class)
+public class TestPcapngRecordReader extends ClusterTest {
+
   @BeforeClass
-  public static void setupTestFiles() {
+  public static void setup() throws Exception {
+    ClusterTest.startCluster(ClusterFixture.builder(dirTestWatcher));
     dirTestWatcher.copyResourceToRoot(Paths.get("store", "pcapng"));
   }
 
   @Test
   public void testStarQuery() throws Exception {
-    Assert.assertEquals(123, testSql("select * from dfs.`store/pcapng/sniff.pcapng`"));
-    Assert.assertEquals(1, testSql("select * from dfs.`store/pcapng/example.pcapng`"));
+    String sql = "select * from dfs.`store/pcapng/sniff.pcapng`";
+    QueryBuilder builder = client.queryBuilder().sql(sql);
+    RowSet sets = builder.rowSet();
+
+    assertEquals(123, sets.rowCount());
+    sets.clear();
   }
 
   @Test
-  public void testProjectingByName() throws Exception {
-    Assert.assertEquals(123, testSql("select `timestamp`, packet_data, type from dfs.`store/pcapng/sniff.pcapng`"));
-    Assert.assertEquals(1, testSql("select src_ip, dst_ip, `timestamp` from dfs.`store/pcapng/example.pcapng`"));
+  public void testExplicitQuery() throws Exception {
+    String sql = "select type, packet_length, `timestamp` from dfs.`store/pcapng/sniff.pcapng` where type = 'ARP'";
+    QueryBuilder builder = client.queryBuilder().sql(sql);
+    RowSet sets = builder.rowSet();
+
+    TupleMetadata schema = new SchemaBuilder()
+        .addNullable("type", MinorType.VARCHAR)
+        .add("packet_length", MinorType.INT)
+        .add("timestamp", MinorType.TIMESTAMP)
+        .buildSchema();
+
+    RowSet expected = new RowSetBuilder(client.allocator(), schema)
+        .addRow("ARP", 90, Instant.ofEpochMilli(1518010669927L))
+        .addRow("ARP", 90, Instant.ofEpochMilli(1518010671874L))
+        .build();
+
+    assertEquals(2, sets.rowCount());
+    new RowSetComparison(expected).verifyAndClearAll(sets);
   }
 
   @Test
-  public void testDiffCaseQuery() throws Exception {
-    Assert.assertEquals(123, testSql("select `timestamp`, paCket_dAta, TyPe from dfs.`store/pcapng/sniff.pcapng`"));
-    Assert.assertEquals(1, testSql("select src_ip, dst_ip, `Timestamp` from dfs.`store/pcapng/example.pcapng`"));
+  public void testLimitPushdown() throws Exception {
+    String sql = "select * from dfs.`store/pcapng/sniff.pcapng` where type = 'UDP' limit 10 offset 65";
+    QueryBuilder builder = client.queryBuilder().sql(sql);
+    RowSet sets = builder.rowSet();
+
+    assertEquals(6, sets.rowCount());
+    sets.clear();
   }
 
   @Test
-  public void testProjectingMissColls() throws Exception {
-    Assert.assertEquals(123, testSql("select `timestamp`, `name`, `color` from dfs.`store/pcapng/sniff.pcapng`"));
-    Assert.assertEquals(1, testSql("select src_ip, `time` from dfs.`store/pcapng/example.pcapng`"));
+  public void testSerDe() throws Exception {
+    String sql = "select count(*) from dfs.`store/pcapng/example.pcapng`";
+    String plan = queryBuilder().sql(sql).explainJson();
+    long cnt = queryBuilder().physical(plan).singletonLong();
+
+    assertEquals("Counts should match", 1, cnt);
   }
 
+  @Test
+  public void testExplicitQueryWithCompressedFile() throws Exception {
+    QueryTestUtil.generateCompressedFile("store/pcapng/sniff.pcapng", "zip", "store/pcapng/sniff.pcapng.zip");
+    String sql = "select type, packet_length, `timestamp` from dfs.`store/pcapng/sniff.pcapng.zip` where type = 'ARP'";
+    QueryBuilder builder = client.queryBuilder().sql(sql);
+    RowSet sets = builder.rowSet();
+
+    TupleMetadata schema = new SchemaBuilder()
+        .addNullable("type", MinorType.VARCHAR)
+        .add("packet_length", MinorType.INT)
+        .add("timestamp", MinorType.TIMESTAMP)
+        .buildSchema();
+
+    RowSet expected = new RowSetBuilder(client.allocator(), schema)
+        .addRow("ARP", 90, Instant.ofEpochMilli(1518010669927L))
+        .addRow("ARP", 90, Instant.ofEpochMilli(1518010671874L))
+        .build();
+
+    assertEquals(2, sets.rowCount());
+    new RowSetComparison(expected).verifyAndClearAll(sets);
+  }
 
   @Test
-  public void testCountQuery() throws Exception {
-    testBuilder()
-        .sqlQuery("select count(*) as ct from dfs.`store/pcapng/sniff.pcapng`")
-        .ordered()
-        .baselineColumns("ct")
-        .baselineValues(123L)
-        .build()
-        .run();
+  public void testCaseInsensitiveQuery() throws Exception {
+    String sql = "select `timestamp`, paCket_dAta, TyPe from dfs.`store/pcapng/sniff.pcapng`";
+    QueryBuilder builder = client.queryBuilder().sql(sql);
+    RowSet sets = builder.rowSet();
 
-    testBuilder()
-        .sqlQuery("select count(*) as ct from dfs.`store/pcapng/example.pcapng`")
-        .ordered()
-        .baselineColumns("ct")
-        .baselineValues(1L)
-        .build()
-        .run();
+    assertEquals(123, sets.rowCount());
+    sets.clear();
+  }
+
+  @Test
+  public void testWhereSyntaxQuery() throws Exception {
+    String sql = "select type, src_ip, dst_ip, packet_length from dfs.`store/pcapng/sniff.pcapng` where src_ip= '10.2.15.239'";
+    QueryBuilder builder = client.queryBuilder().sql(sql);
+    RowSet sets = builder.rowSet();
+
+    TupleMetadata schema = new SchemaBuilder()
+        .addNullable("type", MinorType.VARCHAR)
+        .addNullable("src_ip", MinorType.VARCHAR)
+        .addNullable("dst_ip", MinorType.VARCHAR)
+        .add("packet_length", MinorType.INT)
+        .buildSchema();
+
+    RowSet expected = new RowSetBuilder(client.allocator(), schema)
+        .addRow("UDP", "10.2.15.239", "239.255.255.250", 214)
+        .addRow("UDP", "10.2.15.239", "239.255.255.250", 214)
+        .addRow("UDP", "10.2.15.239", "239.255.255.250", 214)
+        .build();
+
+    assertEquals(3, sets.rowCount());
+    new RowSetComparison(expected).verifyAndClearAll(sets);
+  }
+
+  @Test
+  public void testValidHeaders() throws Exception {
+    String sql = "select * from dfs.`store/pcapng/sniff.pcapng`";
+    RowSet sets = client.queryBuilder().sql(sql).rowSet();
+
+    TupleMetadata schema = new SchemaBuilder()
+        .add("timestamp", MinorType.TIMESTAMP)
+        .add("packet_length", MinorType.INT)
+        .addNullable("type", MinorType.VARCHAR)
+        .addNullable("src_ip", MinorType.VARCHAR)
+        .addNullable("dst_ip", MinorType.VARCHAR)
+        .addNullable("src_port", MinorType.INT)
+        .addNullable("dst_port", MinorType.INT)
+        .addNullable("src_mac_address", MinorType.VARCHAR)
+        .addNullable("dst_mac_address", MinorType.VARCHAR)
+        .addNullable("tcp_session", MinorType.BIGINT)
+        .addNullable("tcp_ack", MinorType.INT)
+        .addNullable("tcp_flags", MinorType.INT)
+        .addNullable("tcp_flags_ns", MinorType.INT)
+        .addNullable("tcp_flags_cwr", MinorType.INT)
+        .addNullable("tcp_flags_ece", MinorType.INT)
+        .addNullable("tcp_flags_ece_ecn_capable", MinorType.INT)
+        .addNullable("tcp_flags_ece_congestion_experienced", MinorType.INT)
+        .addNullable("tcp_flags_urg", MinorType.INT)
+        .addNullable("tcp_flags_ack", MinorType.INT)
+        .addNullable("tcp_flags_psh", MinorType.INT)
+        .addNullable("tcp_flags_rst", MinorType.INT)
+        .addNullable("tcp_flags_syn", MinorType.INT)
+        .addNullable("tcp_flags_fin", MinorType.INT)
+        .addNullable("tcp_parsed_flags", MinorType.VARCHAR)
+        .addNullable("packet_data", MinorType.VARCHAR)
+        .build();
+
+    RowSet expected = new RowSetBuilder(client.allocator(), schema).build();
+    new RowSetComparison(expected).verifyAndClearAll(sets);
   }
 
   @Test
   public void testGroupBy() throws Exception {
-    Assert.assertEquals(47, testSql("select src_ip, count(1), sum(packet_length) from dfs.`store/pcapng/sniff.pcapng` group by src_ip"));
+    String sql = "select src_ip, count(1), sum(packet_length) from dfs.`store/pcapng/sniff.pcapng` group by src_ip";
+    QueryBuilder builder = client.queryBuilder().sql(sql);
+    RowSet sets = builder.rowSet();
+
+    assertEquals(47, sets.rowCount());
+    sets.clear();
   }
 
   @Test
   public void testDistinctQuery() throws Exception {
-    Assert.assertEquals(119, testSql("select distinct `timestamp`, src_ip from dfs.`store/pcapng/sniff.pcapng`"));
-    Assert.assertEquals(1, testSql("select distinct packet_data from dfs.`store/pcapng/example.pcapng`"));
+    String sql = "select distinct `timestamp`, src_ip from dfs.`store/pcapng/sniff.pcapng`";
+    QueryBuilder builder = client.queryBuilder().sql(sql);
+    RowSet sets = builder.rowSet();
+
+    assertEquals(119, sets.rowCount());
+    sets.clear();
   }
 
   @Test(expected = UserRemoteException.class)
   public void testBasicQueryWithIncorrectFileName() throws Exception {
-    testSql("select * from dfs.`store/pcapng/snaff.pcapng`");
-  }
-
-  @Test
-  public void testPhysicalPlanExecutionBasedOnQuery() throws Exception {
-    String query = "EXPLAIN PLAN for select * from dfs.`store/pcapng/sniff.pcapng`";
-    String plan = getPlanInString(query, JSON_FORMAT);
-    Assert.assertEquals(123, testPhysical(plan));
+    String sql = "select * from dfs.`store/pcapng/drill.pcapng`";
+    client.queryBuilder().sql(sql).rowSet();
   }
 }
