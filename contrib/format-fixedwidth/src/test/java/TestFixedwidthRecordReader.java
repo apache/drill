c9014d2a66b7da16bdee2b73a7bf780976198975
Megan Foss <megan@datadistillr.com>
2021-07-29 12:40:27 -0400

    Added more data types, refactored code

--- contrib/format-fixedwidth/src/test/java/TestFixedwidthRecordReader.java
+++ contrib/format-fixedwidth/src/test/java/TestFixedwidthRecordReader.java
@@ -18,6 +18,10 @@ import org.junit.Test;
 import org.junit.experimental.categories.Category;
 
 import java.nio.file.Paths;
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
 
 import static org.junit.Assert.assertEquals;
 
@@ -40,10 +44,12 @@ public class TestFixedwidthRecordReader extends ClusterTest {
             , Lists.newArrayList(
             new FixedwidthFieldConfig(TypeProtos.MinorType.INT, "Number", "", 1, 4),
             new FixedwidthFieldConfig(TypeProtos.MinorType.VARCHAR, "Letter", "", 6, 4),
-            new FixedwidthFieldConfig(TypeProtos.MinorType.INT, "Address", "", 11, 3)
-    ));
+            new FixedwidthFieldConfig(TypeProtos.MinorType.INT,"Address","",11,3),
+            new FixedwidthFieldConfig(TypeProtos.MinorType.DATE,"Date","MM-dd-yyyy",15,10),
+            new FixedwidthFieldConfig(TypeProtos.MinorType.TIME,"Time","HH:mm:ss",26,8),
+            new FixedwidthFieldConfig(TypeProtos.MinorType.TIMESTAMP,"DateTime","MM-dd-yyyy'T'HH:mm:ss.SSX",35,23)
+     ));
     cluster.defineFormat("cp", "fwf", formatConfig);
-    //cluster.defineFormat("dfs", "xml", formatConfig);
 
     // Needed for compressed file unit test
     dirTestWatcher.copyResourceToRoot(Paths.get("fwf/"));
@@ -75,13 +81,6 @@ public class TestFixedwidthRecordReader extends ClusterTest {
 
   @Test
   public void testBatchReader() throws Exception {
-    FixedwidthFieldConfig testField = new FixedwidthFieldConfig(TypeProtos.MinorType.FLOAT8, "date", "MM/DD/YYYY", 1, 10);
-    System.out.println(testField.getFieldName());
-    System.out.println(testField.getStartIndex());
-    System.out.println(testField.getFieldWidth());
-    System.out.println(testField.getDateTimeFormat());
-    System.out.println(testField.getDataType());
-
     String sql = "SELECT * FROM cp.`fwf/test.fwf`";
     RowSet results = client.queryBuilder().sql(sql).rowSet();
 
@@ -89,16 +88,46 @@ public class TestFixedwidthRecordReader extends ClusterTest {
             .addNullable("Number", TypeProtos.MinorType.INT)
             .addNullable("Letter", TypeProtos.MinorType.VARCHAR)
             .addNullable("Address", TypeProtos.MinorType.INT)
+            .addNullable("Date", TypeProtos.MinorType.DATE)
+            .addNullable("Time",TypeProtos.MinorType.TIME)
+            .addNullable("DateTime",TypeProtos.MinorType.TIMESTAMP)
             .buildSchema();
 
 
     RowSet expected = new RowSetBuilder(client.allocator(), expectedSchema)
-            .addRow(1234, "test", 567)
+            .addRow(1234, "test", 567, LocalDate.parse("2021-02-10"), LocalTime.parse("10:30:27"), Instant.parse("2021-02-10T15:30:27.00Z"))
+            .addRow(5678, "TEST", 890, LocalDate.parse("2021-07-27"), LocalTime.parse("12:40:15"), Instant.parse("2021-07-27T16:40:15.00Z"))
+            .addRow(1111, "abcd", 111, LocalDate.parse("1111-11-11"), LocalTime.parse("11:11:11"), Instant.parse("1111-11-11T16:28:43.11Z"))
+            .addRow(2222, "efgh", 222, LocalDate.parse("2222-01-22"), LocalTime.parse("22:22:22"), Instant.parse("2222-01-23T03:22:22.22Z"))
+            .addRow(3333, "ijkl", 333, LocalDate.parse("3333-02-01"), LocalTime.parse("01:33:33"), Instant.parse("3333-02-01T06:33:33.33Z"))
+            .addRow(4444, "mnop", 444, LocalDate.parse("4444-03-02"), LocalTime.parse("02:44:44"), Instant.parse("4444-03-02T07:44:44.44Z"))
+            .addRow(5555, "qrst", 555, LocalDate.parse("5555-04-03"), LocalTime.parse("03:55:55"), Instant.parse("5555-04-03T07:55:55.55Z"))
+            .addRow(6666, "uvwx", 666, LocalDate.parse("6666-05-04"), LocalTime.parse("04:01:01"), Instant.parse("6666-05-04T08:01:01.01Z"))
+            .addRow(7777, "yzzz", 777, LocalDate.parse("7777-06-05"), LocalTime.parse("05:11:11"), Instant.parse("7777-06-05T09:11:11.11Z"))
+            .addRow(8888, "aabb", 888, LocalDate.parse("8888-07-06"), LocalTime.parse("06:22:22"), Instant.parse("8888-07-07T10:22:22.22Z"))
+            .addRow(8888, "aabb", 888, LocalDate.parse("8888-07-06"), LocalTime.parse("06:22:22"), Instant.parse("8888-07-07T10:22:22.22Z"))
+            .addRow(8888, "aabb", 888, LocalDate.parse("8888-07-06"), LocalTime.parse("06:22:22"), Instant.parse("8888-07-07T10:22:22.22Z"))
+            .addRow(8888, "aabb", 888, LocalDate.parse("8888-07-06"), LocalTime.parse("06:22:22"), Instant.parse("8888-07-07T10:22:22.22Z"))
+            .addRow(8888, "aabb", 888, LocalDate.parse("8888-07-06"), LocalTime.parse("06:22:22"), Instant.parse("8888-07-07T10:22:22.22Z"))
+            .addRow(8888, "aabb", 888, LocalDate.parse("8888-07-06"), LocalTime.parse("06:22:22"), Instant.parse("8888-07-07T10:22:22.22Z"))
+            .addRow(8888, "aabb", 888, LocalDate.parse("8888-07-06"), LocalTime.parse("06:22:22"), Instant.parse("8888-07-07T10:22:22.22Z"))
+            .addRow(8888, "aabb", 888, LocalDate.parse("8888-07-06"), LocalTime.parse("06:22:22"), Instant.parse("8888-07-07T10:22:22.22Z"))
+            .addRow(8888, "aabb", 888, LocalDate.parse("8888-07-06"), LocalTime.parse("06:22:22"), Instant.parse("8888-07-07T10:22:22.22Z"))
+            .addRow(8888, "aabb", 888, LocalDate.parse("8888-07-06"), LocalTime.parse("06:22:22"), Instant.parse("8888-07-07T10:22:22.22Z"))
+            .addRow(8888, "aabb", 888, LocalDate.parse("8888-07-06"), LocalTime.parse("06:22:22"), Instant.parse("8888-07-07T10:22:22.22Z"))
+            .addRow(8888, "aabb", 888, LocalDate.parse("8888-07-06"), LocalTime.parse("06:22:22"), Instant.parse("8888-07-07T10:22:22.22Z"))
+            .addRow(8888, "aabb", 888, LocalDate.parse("8888-07-06"), LocalTime.parse("06:22:22"), Instant.parse("8888-07-07T10:22:22.22Z"))
+            .addRow(8888, "aabb", 888, LocalDate.parse("8888-07-06"), LocalTime.parse("06:22:22"), Instant.parse("8888-07-07T10:22:22.22Z"))
+            .addRow(8888, "aabb", 888, LocalDate.parse("8888-07-06"), LocalTime.parse("06:22:22"), Instant.parse("8888-07-07T10:22:22.22Z"))
+            .addRow(8888, "aabb", 888, LocalDate.parse("8888-07-06"), LocalTime.parse("06:22:22"), Instant.parse("8888-07-07T10:22:22.22Z"))
             .build();
 
-    assertEquals(1, results.rowCount());
+    System.out.println(expected);
+    assertEquals(25, results.rowCount());
 
-    System.out.println(results.batchSchema());
+    //System.out.println(results.batchSchema());
+    System.out.println(results);
+
 
     new RowSetComparison(expected).verifyAndClearAll(results);
     System.out.println("Test complete.");
