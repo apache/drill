a79f8a5536541f0856f6346fbe45b2a8937362a4
Megan Foss <megan@datadistillr.com>
2021-11-23 16:14:24 -0500

    Added field validation for data types, indices, width. Includes creating two setters in field config to set default value for data types and calculate/set width based on indices.

--- contrib/format-fixedwidth/src/main/java/org/apache/drill/exec/store/fixedwidth/FixedwidthFieldConfig.java
+++ contrib/format-fixedwidth/src/main/java/org/apache/drill/exec/store/fixedwidth/FixedwidthFieldConfig.java
@@ -18,6 +18,7 @@
 
 package org.apache.drill.exec.store.fixedwidth;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonTypeName;
@@ -29,12 +30,12 @@ import java.util.Objects;
 
 @JsonTypeName("fixedwidthReaderFieldDescription")
 @JsonInclude(JsonInclude.Include.NON_DEFAULT)
-public class FixedwidthFieldConfig {
+public class FixedwidthFieldConfig implements Comparable<FixedwidthFieldConfig> {
 
   private final String name;
   private final int index;
-  private final int width;
-  private final TypeProtos.MinorType type;
+  private int width;
+  private TypeProtos.MinorType type;
   private final String dateTimeFormat;
 
   public FixedwidthFieldConfig(@JsonProperty("name") String name,
@@ -44,41 +45,17 @@ public class FixedwidthFieldConfig {
     this(name, index, width, type, null);
   }
 
+  @JsonCreator
   public FixedwidthFieldConfig(@JsonProperty("name") String name,
                                @JsonProperty("index") int index,
                                @JsonProperty("width") int width,
                                @JsonProperty("type") TypeProtos.MinorType type,
                                @JsonProperty("dateTimeFormat") String dateTimeFormat) {
-
     this.name = name;
     this.index = index;
     this.width = width;
     this.type = type;
     this.dateTimeFormat = dateTimeFormat;
-
-
-    // Need to verify names are different - where can we access all the names of other columns
-//    if(name != null){
-//      this.name = name;
-//    } else{
-//      throw new IllegalArgumentException("Invalid name"); //Is this the right way to throw an exception if blank? What about if not valid SQL?
-//    }
-//
-//    if (index >= 0){
-//      this.index = index;
-//    } else {
-//        throw new IllegalArgumentException("Index must be 0 or greater");
-//    }
-//
-//    //Can modify this to be optional and be calculated based on start index of this field and next
-//    this.width = width;
-//
-//    if (type == null){
-//      this.type = TypeProtos.MinorType.VARCHAR;
-//    } else {
-//      this.type = type;
-//    }
-//    this.dateTimeFormat = dateTimeFormat; // No default required, null is allowed
   }
 
   public String getName() {return name;}
@@ -87,7 +64,15 @@ public class FixedwidthFieldConfig {
 
   public int getWidth() {return width;}
 
+  public void setWidth(int value) {
+    this.width = value;
+  }
+
   public TypeProtos.MinorType getType() {return type;}
+
+  public void setType() {
+    this.type = TypeProtos.MinorType.VARCHAR;
+  }
 
   public String getDateTimeFormat() {return dateTimeFormat;}
 
@@ -121,5 +106,10 @@ public class FixedwidthFieldConfig {
       .field("type", type)
       .field("dateTimeFormat", dateTimeFormat)
       .toString();
+  }
+
+  @Override
+  public int compareTo(FixedwidthFieldConfig o) {
+    return new Integer(this.getIndex()).compareTo(o.getIndex());
   }
 }
