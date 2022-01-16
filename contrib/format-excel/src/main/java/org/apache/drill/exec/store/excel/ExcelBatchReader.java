ad95a42aec80ee50da58b63b1bc48ffeca07396c
Charles Givre <cgivre@apache.org>
2022-01-15 20:22:58 -0500

    Used TreeSet

--- contrib/format-excel/src/main/java/org/apache/drill/exec/store/excel/ExcelBatchReader.java
+++ contrib/format-excel/src/main/java/org/apache/drill/exec/store/excel/ExcelBatchReader.java
@@ -58,6 +58,7 @@ import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
+import java.util.TreeSet;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -142,6 +143,7 @@ public class ExcelBatchReader implements ManagedReader<FileSchemaNegotiator> {
 
   private final ExcelReaderConfig readerConfig;
   private final int maxRecords;
+  private final TreeSet<String> columnNameChecker;
   private Sheet sheet;
   private Row currentRow;
   private StreamingWorkbook streamingWorkbook;
@@ -161,6 +163,7 @@ public class ExcelBatchReader implements ManagedReader<FileSchemaNegotiator> {
   private Map<String, Date> dateMetadata;
   private Map<String, List<String>> listMetadata;
   private CustomErrorContext errorContext;
+
 
 
   static class ExcelReaderConfig {
@@ -186,6 +189,7 @@ public class ExcelBatchReader implements ManagedReader<FileSchemaNegotiator> {
   public ExcelBatchReader(ExcelReaderConfig readerConfig, int maxRecords) {
     this.readerConfig = readerConfig;
     this.maxRecords = maxRecords;
+    this.columnNameChecker = new TreeSet<>(String.CASE_INSENSITIVE_ORDER);
     firstLine = true;
   }
 
@@ -351,6 +355,7 @@ public class ExcelBatchReader implements ManagedReader<FileSchemaNegotiator> {
             tempColumnName = deconflictColumnNames(tempColumnName);
             makeColumn(builder, tempColumnName, MinorType.VARCHAR);
             excelFieldNames.add(colPosition, tempColumnName);
+            columnNameChecker.add(tempColumnName);
             break;
           default:
             tempColumnName = cell.getStringCellValue();
@@ -360,6 +365,7 @@ public class ExcelBatchReader implements ManagedReader<FileSchemaNegotiator> {
             tempColumnName = deconflictColumnNames(tempColumnName);
             makeColumn(builder, tempColumnName, MinorType.FLOAT8);
             excelFieldNames.add(colPosition, tempColumnName);
+            columnNameChecker.add(tempColumnName);
             break;
         }
       }
@@ -377,7 +383,7 @@ public class ExcelBatchReader implements ManagedReader<FileSchemaNegotiator> {
   private String deconflictColumnNames(String columnName) {
     Pattern pattern = Pattern.compile("_(\\d+)$");
     Matcher matcher = pattern.matcher(columnName);
-    while (containsIgnoreCase(columnName, excelFieldNames)) {
+    while (columnNameChecker.contains(columnName)) {
       if (matcher.find()) {
         int index = Integer.parseInt(matcher.group(1));
         index++;
@@ -387,15 +393,6 @@ public class ExcelBatchReader implements ManagedReader<FileSchemaNegotiator> {
       }
     }
     return columnName;
-  }
-
-  private boolean containsIgnoreCase(String str, List<String> list) {
-    for (String i : list) {
-      if (i.equalsIgnoreCase(str)) {
-        return true;
-      }
-    }
-    return false;
   }
 
   /**
