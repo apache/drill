80fa2662ce207a75e7822b7e5de816cd2c6461ce
Thinking Chen <cdmikechen@hotmail.com>
2022-02-02 17:34:47 +0800

    DRILL-8109: Hive storage plugin support reading parquet timestamp type with int64 logical type (#2431)

--- contrib/storage-hive/core/src/main/java/org/apache/drill/exec/store/hive/writers/primitive/HiveTimestampWriter.java
+++ contrib/storage-hive/core/src/main/java/org/apache/drill/exec/store/hive/writers/primitive/HiveTimestampWriter.java
@@ -20,7 +20,9 @@ package org.apache.drill.exec.store.hive.writers.primitive;
 import org.apache.drill.exec.vector.complex.writer.TimeStampWriter;
 import org.apache.hadoop.hive.serde2.objectinspector.PrimitiveObjectInspector;
 import org.apache.hadoop.hive.serde2.objectinspector.primitive.PrimitiveObjectInspectorUtils;
+import org.apache.hadoop.io.LongWritable;
 import org.joda.time.DateTime;
+import org.joda.time.DateTimeConstants;
 import org.joda.time.DateTimeZone;
 
 import java.sql.Timestamp;
@@ -33,10 +35,14 @@ public class HiveTimestampWriter extends AbstractSingleValueWriter<PrimitiveObje
 
   @Override
   public void write(Object value) {
-    String timestampString = PrimitiveObjectInspectorUtils.getString(value, inspector);
-    long timestampMillis = new DateTime(Timestamp.valueOf(timestampString).getTime())
-        .withZoneRetainFields(DateTimeZone.UTC).getMillis();
-    writer.writeTimeStamp(timestampMillis);
+    if (value instanceof LongWritable) {
+      writer.writeTimeStamp(((LongWritable) value).get() / DateTimeConstants.MILLIS_PER_SECOND);
+    } else {
+      String timestampString = PrimitiveObjectInspectorUtils.getString(value, inspector);
+      long timestampMillis = new DateTime(Timestamp.valueOf(timestampString).getTime())
+          .withZoneRetainFields(DateTimeZone.UTC).getMillis();
+      writer.writeTimeStamp(timestampMillis);
+    }
   }
 
 }
