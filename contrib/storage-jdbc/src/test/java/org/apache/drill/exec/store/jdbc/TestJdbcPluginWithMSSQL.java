ce291a8ede25a78218d1bb832b3fcac6f80cc67d
James Turton <james@somecomputer.xyz>
2022-05-22 16:25:01 +0200

    Try using DisabledIf... instead of EnabledIf.. at class level.

--- contrib/storage-jdbc/src/test/java/org/apache/drill/exec/store/jdbc/TestJdbcPluginWithMSSQL.java
+++ contrib/storage-jdbc/src/test/java/org/apache/drill/exec/store/jdbc/TestJdbcPluginWithMSSQL.java
@@ -34,7 +34,7 @@ import org.junit.BeforeClass;
 import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.experimental.categories.Category;
-import org.junit.jupiter.api.condition.EnabledIfSystemProperty;
+import org.junit.jupiter.api.condition.DisabledIfSystemProperty;
 import org.testcontainers.containers.MSSQLServerContainer;
 
 import java.math.BigDecimal;
@@ -47,7 +47,7 @@ import static org.junit.Assert.assertEquals;
  * JDBC storage plugin tests against MSSQL.
  */
 @Category(JdbcStorageTest.class)
-@EnabledIfSystemProperty(named = "os.arch", matches = "(amd64|x86_64)")
+@DisabledIfSystemProperty(named = "os.arch", matches = "aarch64")
 public class TestJdbcPluginWithMSSQL extends ClusterTest {
 
   private static MSSQLServerContainer jdbcContainer;
