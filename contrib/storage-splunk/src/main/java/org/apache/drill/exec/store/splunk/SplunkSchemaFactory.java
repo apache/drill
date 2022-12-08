82c690f38f5d14d6ae23e941a30a086725a18434
Charles Givre <cgivre@apache.org>
2022-12-08 11:47:50 -0500

    WIP

--- contrib/storage-splunk/src/main/java/org/apache/drill/exec/store/splunk/SplunkSchemaFactory.java
+++ contrib/storage-splunk/src/main/java/org/apache/drill/exec/store/splunk/SplunkSchemaFactory.java
@@ -19,32 +19,11 @@
 package org.apache.drill.exec.store.splunk;
 
 import org.apache.calcite.schema.SchemaPlus;
-import org.apache.calcite.schema.Table;
-import org.apache.drill.common.exceptions.UserException;
-import org.apache.drill.exec.physical.base.PhysicalOperator;
-import org.apache.drill.exec.physical.base.Writer;
-import org.apache.drill.exec.planner.logical.CreateTableEntry;
-import org.apache.drill.exec.planner.logical.DynamicDrillTable;
-import org.apache.drill.exec.planner.logical.ModifyTableEntry;
-import org.apache.drill.exec.store.AbstractSchema;
 import org.apache.drill.exec.store.AbstractSchemaFactory;
 import org.apache.drill.exec.store.SchemaConfig;
-import org.apache.drill.exec.store.StorageStrategy;
-import org.apache.drill.shaded.guava.com.google.common.collect.Sets;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-import java.io.IOException;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
 
 public class SplunkSchemaFactory extends AbstractSchemaFactory {
 
-  private static final Logger logger = LoggerFactory.getLogger(SplunkSchemaFactory.class);
-  private static final String SPL_TABLE_NAME = "spl";
   private final SplunkStoragePlugin plugin;
   private String queryUserName;
 
@@ -58,111 +37,5 @@ public class SplunkSchemaFactory extends AbstractSchemaFactory {
     this.queryUserName = schemaConfig.getUserName();
     SplunkSchema schema = new SplunkSchema(plugin, queryUserName);
     SchemaPlus plusOfThis = parent.add(schema.getName(), schema);
-  }
-
-  static class SplunkSchema extends AbstractSchema {
-
-    private final Map<String, DynamicDrillTable> activeTables = new HashMap<>();
-    private final SplunkStoragePlugin plugin;
-    private final String queryUserName;
-
-    public SplunkSchema(SplunkStoragePlugin plugin, String queryUserName) {
-      super(Collections.emptyList(), plugin.getName());
-      this.plugin = plugin;
-      this.queryUserName = queryUserName;
-
-
-      registerIndexes();
-    }
-
-    @Override
-    public Table getTable(String name) {
-      DynamicDrillTable table = activeTables.get(name);
-      if (table != null) {
-        // If the table was found, return it.
-        return table;
-      } else {
-        // Register the table
-        return registerTable(name, new DynamicDrillTable(plugin, plugin.getName(),
-          new SplunkScanSpec(plugin.getName(), name, plugin.getConfig(), queryUserName)));
-      }
-    }
-
-    @Override
-    public boolean showInInformationSchema() {
-      return true;
-    }
-
-    @Override
-    public Set<String> getTableNames() {
-      return Sets.newHashSet(activeTables.keySet());
-    }
-
-    private DynamicDrillTable registerTable(String name, DynamicDrillTable table) {
-      activeTables.put(name, table);
-      return table;
-    }
-
-    @Override
-    public CreateTableEntry createNewTable(String tableName, List<String> partitionColumns,
-      StorageStrategy strategy) {
-      if (plugin.getConfig().isWritable() == null || (! plugin.getConfig().isWritable())) {
-        throw UserException
-          .dataWriteError()
-          .message(plugin.getName() + " is not writable.")
-          .build(logger);
-      }
-
-      return new CreateTableEntry() {
-        @Override
-        public Writer getWriter(PhysicalOperator child) throws IOException {
-          return new SplunkWriter(child, tableName, null, plugin);
-        }
-
-        @Override
-        public List<String> getPartitionColumns() {
-          return Collections.emptyList();
-        }
-      };
-    }
-
-    @Override
-    public ModifyTableEntry modifyTable(String tableName) {
-      return null;
-      //return child -> new JdbcInsertWriter(child, getFullTablePath(tableName), inner, plugin);
-    }
-
-
-    @Override
-    public String getTypeName() {
-      return SplunkPluginConfig.NAME;
-    }
-
-    private void registerIndexes() {
-      // Verify that the connection is successful.  If not, don't register any indexes,
-      // and throw an exception.
-      SplunkPluginConfig config = plugin.getConfig();
-      SplunkConnection connection;
-      try {
-        connection = new SplunkConnection(config, queryUserName);
-        connection.connect();
-      } catch (Exception e) {
-        // Catch any connection errors that may happen.
-        throw UserException.connectionError()
-          .message("Unable to connect to Splunk: " +  plugin.getName() + " " + e.getMessage())
-          .build(logger);
-      }
-
-      // Add default "spl" table to index list.
-      registerTable(SPL_TABLE_NAME, new DynamicDrillTable(plugin, plugin.getName(),
-        new SplunkScanSpec(plugin.getName(), SPL_TABLE_NAME, plugin.getConfig(), queryUserName)));
-
-      // Retrieve and add all other Splunk indexes
-      for (String indexName : connection.getIndexes().keySet()) {
-        logger.debug("Registering {}", indexName);
-        registerTable(indexName, new DynamicDrillTable(plugin, plugin.getName(),
-          new SplunkScanSpec(plugin.getName(), indexName, config, queryUserName)));
-      }
-    }
   }
 }
