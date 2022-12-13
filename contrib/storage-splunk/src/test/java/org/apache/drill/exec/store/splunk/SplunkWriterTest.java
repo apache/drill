77a3c3ddd97760081986f90c88c77c8d61c77222
Charles Givre <cgivre@apache.org>
2022-12-13 09:30:08 -0500

    Added UT for insert

--- contrib/storage-splunk/src/test/java/org/apache/drill/exec/store/splunk/SplunkWriterTest.java
+++ contrib/storage-splunk/src/test/java/org/apache/drill/exec/store/splunk/SplunkWriterTest.java
@@ -1,3 +1,21 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.apache.drill.exec.store.splunk;
 
 import org.apache.drill.categories.SlowTest;
@@ -121,5 +139,34 @@ public class SplunkWriterTest extends SplunkBaseTest {
     String dropQuery = "DROP TABLE splunk.t2";
     QuerySummary dropResults = queryBuilder().sql(dropQuery).run();
     assertTrue(dropResults.succeeded());
+  }
+
+  // TODO Add field converters for repeated and complex objects
+
+  @Test
+  public void testInsert() throws Exception {
+
+    // Now create the table
+    String sql = "CREATE TABLE `splunk`.`t1` AS SELECT * FROM cp.`test_data.csvh`";
+    QuerySummary summary = client.queryBuilder().sql(sql).run();
+    assertTrue(summary.succeeded());
+
+    sql = "INSERT INTO `splunk`.`t1`  SELECT * FROM cp.`test_data.csvh`";
+    summary = client.queryBuilder().sql(sql).run();
+    assertTrue(summary.succeeded());
+
+    // There seems to be some delay between the Drill query writing the data and the data being made
+    // accessible.
+    Thread.sleep(15000);
+
+    // Next verify that the results arrived.
+    sql = "SELECT COUNT(*) as row_count FROM splunk.`t1`";
+    long resultCount = client.queryBuilder().sql(sql).singletonLong();
+    assertEquals(10L, resultCount);
+
+    // Now drop the index
+    sql = "DROP TABLE splunk.`t1`";
+    summary = client.queryBuilder().sql(sql).run();
+    assertTrue(summary.succeeded());
   }
 }
