a7b7735c51cd92667f7ac023044ad7b3fbeb8002
Charles S. Givre <cgivre@apache.org>
2023-02-07 07:21:59 -0500

    DRILL-8387: Add Support for User Translation to ElasticSearch Plugin (#2739)

--- contrib/storage-elasticsearch/src/test/java/org/apache/drill/exec/store/elasticsearch/ElasticInfoSchemaTest.java
+++ contrib/storage-elasticsearch/src/test/java/org/apache/drill/exec/store/elasticsearch/ElasticInfoSchemaTest.java
@@ -17,33 +17,33 @@
  */
 package org.apache.drill.exec.store.elasticsearch;
 
-import org.apache.drill.common.logical.security.PlainCredentialsProvider;
+import org.apache.drill.common.logical.StoragePluginConfig.AuthMode;
+import org.apache.drill.shaded.guava.com.google.common.collect.ImmutableMap;
 import org.apache.drill.test.ClusterFixture;
 import org.apache.drill.test.ClusterTest;
-import org.apache.http.HttpHost;
-import org.elasticsearch.action.admin.indices.delete.DeleteIndexRequest;
-import org.elasticsearch.action.admin.indices.refresh.RefreshRequest;
-import org.elasticsearch.action.index.IndexRequest;
-import org.elasticsearch.client.RequestOptions;
-import org.elasticsearch.client.RestClient;
-import org.elasticsearch.client.RestHighLevelClient;
-import org.elasticsearch.client.indices.CreateIndexRequest;
-import org.elasticsearch.xcontent.XContentBuilder;
-import org.elasticsearch.xcontent.XContentFactory;
+import org.json.simple.JSONObject;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
+import co.elastic.clients.elasticsearch.ElasticsearchClient;
+import co.elastic.clients.elasticsearch.core.IndexRequest;
+import co.elastic.clients.elasticsearch.indices.CreateIndexRequest;
+import co.elastic.clients.elasticsearch.indices.DeleteIndexRequest;
+import co.elastic.clients.elasticsearch.indices.RefreshRequest;
+import co.elastic.clients.json.JsonData;
+
 import java.io.IOException;
-import java.util.ArrayList;
+import java.io.Reader;
+import java.io.StringReader;
 import java.util.Collections;
+import java.util.LinkedList;
 import java.util.List;
 
 public class ElasticInfoSchemaTest extends ClusterTest {
 
-  private static final List<String> indexNames = new ArrayList<>();
-
-  public static RestHighLevelClient restHighLevelClient;
+  private static final List<String> indexNames = new LinkedList<>();
+  private static ElasticsearchClient elasticsearchClient;
 
   @BeforeClass
   public static void init() throws Exception {
@@ -52,53 +52,71 @@ public class ElasticInfoSchemaTest extends ClusterTest {
 
     ElasticsearchStorageConfig config = new ElasticsearchStorageConfig(
         Collections.singletonList(TestElasticsearchSuite.getAddress()),
-        null, null, null, PlainCredentialsProvider.EMPTY_CREDENTIALS_PROVIDER);
+        TestElasticsearchSuite.ELASTICSEARCH_USERNAME,
+        TestElasticsearchSuite.ELASTICSEARCH_PASSWORD,
+        null, AuthMode.SHARED_USER.name(),
+        null
+    );
+
     config.setEnabled(true);
     cluster.defineStoragePlugin("elastic", config);
 
+    elasticsearchClient = TestElasticsearchSuite.getESClient();
     prepareData();
   }
 
   @AfterClass
   public static void cleanUp() throws IOException {
-    for (String indexName : indexNames) {
-      restHighLevelClient.indices().delete(new DeleteIndexRequest(indexName), RequestOptions.DEFAULT);
-    }
+    DeleteIndexRequest deleteIndexRequest = new DeleteIndexRequest.Builder()
+      .index(indexNames)
+      .build();
+
+    elasticsearchClient.indices().delete(deleteIndexRequest);
     TestElasticsearchSuite.tearDownCluster();
   }
 
   private static void prepareData() throws IOException {
-    restHighLevelClient = new RestHighLevelClient(RestClient.builder(HttpHost.create(TestElasticsearchSuite.getAddress())));
+    {
+      indexNames.add("t1");
+      CreateIndexRequest createIndexRequest = new CreateIndexRequest.Builder()
+          .index("t1")
+          .build();
+      elasticsearchClient.indices().create(createIndexRequest);
 
-    String indexName = "t1";
-    indexNames.add(indexName);
-    CreateIndexRequest createIndexRequest = new CreateIndexRequest(indexName);
+      final Reader input1 = new StringReader(
+        JSONObject.toJSONString(
+          ImmutableMap.of("string_field", "a", "int_field", 123)
+        )
+      );
+      IndexRequest<JsonData> request = IndexRequest.of(i -> i
+          .index("t1")
+          .withJson(input1)
+      );
+      elasticsearchClient.index(request);
+    }
+    {
+      indexNames.add("t2");
+      CreateIndexRequest createIndexRequest = new CreateIndexRequest.Builder()
+          .index("t2")
+          .build();
+      elasticsearchClient.indices().create(createIndexRequest);
 
-    restHighLevelClient.indices().create(createIndexRequest, RequestOptions.DEFAULT);
+      final Reader input2 = new StringReader(
+        JSONObject.toJSONString(
+          ImmutableMap.of("another_string_field", "b", "another_int_field", 321)
+        )
+      );
+      IndexRequest<JsonData> request = IndexRequest.of(i -> i
+          .index("t2")
+          .withJson(input2)
+      );
+      elasticsearchClient.index(request);
 
-    XContentBuilder builder = XContentFactory.jsonBuilder();
-    builder.startObject();
-    builder.field("string_field", "a");
-    builder.field("int_field", 123);
-    builder.endObject();
-    IndexRequest indexRequest = new IndexRequest(indexName).source(builder);
-    restHighLevelClient.index(indexRequest, RequestOptions.DEFAULT);
-    restHighLevelClient.indices().refresh(new RefreshRequest(indexName), RequestOptions.DEFAULT);
-
-    indexName = "t2";
-    indexNames.add(indexName);
-    createIndexRequest = new CreateIndexRequest(indexName);
-
-    restHighLevelClient.indices().create(createIndexRequest, RequestOptions.DEFAULT);
-
-    builder = XContentFactory.jsonBuilder();
-    builder.startObject();
-    builder.field("another_int_field", 321);
-    builder.field("another_string_field", "b");
-    builder.endObject();
-    indexRequest = new IndexRequest(indexName).source(builder);
-    restHighLevelClient.index(indexRequest, RequestOptions.DEFAULT);
-    restHighLevelClient.indices().refresh(new RefreshRequest(indexName), RequestOptions.DEFAULT);
+      RefreshRequest refreshRequest = new RefreshRequest.Builder()
+          .index(indexNames)
+          .build();
+      elasticsearchClient.indices().refresh(refreshRequest);
+    }
   }
 
   @Test
--- contrib/storage-elasticsearch/src/test/java/org/apache/drill/exec/store/elasticsearch/TestElasticsearchSuite.java
+++ contrib/storage-elasticsearch/src/test/java/org/apache/drill/exec/store/elasticsearch/TestElasticsearchSuite.java
@@ -17,32 +17,65 @@
  */
 package org.apache.drill.exec.store.elasticsearch;
 
+import co.elastic.clients.elasticsearch.ElasticsearchClient;
+import co.elastic.clients.json.jackson.JacksonJsonpMapper;
+import co.elastic.clients.transport.ElasticsearchTransport;
+import co.elastic.clients.transport.rest_client.RestClientTransport;
+import org.apache.commons.io.IOUtils;
 import org.apache.drill.categories.SlowTest;
 import org.apache.drill.test.BaseTest;
+
+import org.apache.http.HttpHost;
+import org.apache.http.auth.AuthScope;
+import org.apache.http.auth.UsernamePasswordCredentials;
+import org.apache.http.client.CredentialsProvider;
+import org.apache.http.impl.client.BasicCredentialsProvider;
+import org.apache.http.ssl.SSLContextBuilder;
+import org.apache.http.ssl.SSLContexts;
+import org.elasticsearch.client.RestClient;
+import org.elasticsearch.client.RestClientBuilder;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.experimental.categories.Category;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.testcontainers.elasticsearch.ElasticsearchContainer;
-import org.testcontainers.utility.DockerImageName;
 
+import com.google.api.client.util.SslUtils;
+
+import javax.net.ssl.HttpsURLConnection;
+import javax.net.ssl.SSLContext;
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.security.GeneralSecurityException;
+import java.security.KeyStore;
+import java.security.cert.Certificate;
+import java.security.cert.CertificateFactory;
 import java.time.Duration;
 import java.util.concurrent.atomic.AtomicInteger;
 
+
 @Category(SlowTest.class)
 @RunWith(Suite.class)
-@Suite.SuiteClasses({ElasticComplexTypesTest.class, ElasticInfoSchemaTest.class, ElasticSearchPlanTest.class, ElasticSearchQueryTest.class})
+@Suite.SuiteClasses({
+    ElasticComplexTypesTest.class,
+    ElasticInfoSchemaTest.class,
+    ElasticSearchPlanTest.class,
+    ElasticSearchQueryTest.class,
+    ElasticSearchUserTranslationTest.class})
 public class TestElasticsearchSuite extends BaseTest {
 
   protected static ElasticsearchContainer elasticsearch;
+  public static final String ELASTICSEARCH_USERNAME = "elastic";
+  public static final String ELASTICSEARCH_PASSWORD = "s3cret";
+  private static final String IMAGE_NAME = "docker.elastic.co/elasticsearch/elasticsearch:8.6.0";
 
   private static final AtomicInteger initCount = new AtomicInteger(0);
 
   private static volatile boolean runningSuite = false;
 
   @BeforeClass
-  public static void initElasticsearch() {
+  public static void initElasticsearch() throws IOException, GeneralSecurityException {
     synchronized (TestElasticsearchSuite.class) {
       if (initCount.get() == 0) {
         startElasticsearch();
@@ -52,25 +85,89 @@ public class TestElasticsearchSuite extends BaseTest {
     }
   }
 
-  public static boolean isRunningSuite() {
-    return runningSuite;
-  }
-
   @AfterClass
   public static void tearDownCluster() {
     synchronized (TestElasticsearchSuite.class) {
       if (initCount.decrementAndGet() == 0 && elasticsearch != null) {
         elasticsearch.stop();
+        elasticsearch.close();
       }
     }
   }
 
-  private static void startElasticsearch() {
-    DockerImageName imageName = DockerImageName.parse("elasticsearch:7.14.2")
-      .asCompatibleSubstituteFor("docker.elastic.co/elasticsearch/elasticsearch");
-    TestElasticsearchSuite.elasticsearch = new ElasticsearchContainer(imageName)
-      .withStartupTimeout(Duration.ofMinutes(2));
-    TestElasticsearchSuite.elasticsearch.start();
+  public static boolean isRunningSuite() {
+    return runningSuite;
+  }
+
+  /**
+   * Returns an {@link CredentialsProvider} for use with the ElasticSearch test container.
+   * @return A {@link CredentialsProvider} with the default credentials.
+   */
+  public static CredentialsProvider getCredentialsProvider() {
+    CredentialsProvider credentialsProvider = new BasicCredentialsProvider();
+    credentialsProvider.setCredentials(AuthScope.ANY,
+        new UsernamePasswordCredentials(ELASTICSEARCH_USERNAME, ELASTICSEARCH_PASSWORD));
+    return credentialsProvider;
+  }
+
+  public static byte[] getCertAsBytes(ElasticsearchContainer container) {
+    return container.copyFileFromContainer(
+        "/usr/share/elasticsearch/config/certs/http_ca.crt",
+        IOUtils::toByteArray);
+  }
+
+  public static SSLContext createContextFromCaCert(byte[] certAsBytes) {
+    try {
+      CertificateFactory factory = CertificateFactory.getInstance("X.509");
+      Certificate trustedCa = factory.generateCertificate(
+          new ByteArrayInputStream(certAsBytes)
+      );
+      KeyStore trustStore = KeyStore.getInstance("pkcs12");
+      trustStore.load(null, null);
+      trustStore.setCertificateEntry("ca", trustedCa);
+      SSLContextBuilder sslContextBuilder =
+          SSLContexts.custom().loadTrustMaterial(trustStore, null);
+      return sslContextBuilder.build();
+    } catch (Exception e) {
+      throw new RuntimeException(e);
+    }
+  }
+
+  public static ElasticsearchClient getESClient() {
+    HttpHost host = new HttpHost(
+      elasticsearch.getHost(),
+      elasticsearch.getMappedPort(9200),
+      "http"
+    );
+
+    final RestClientBuilder builder = RestClient.builder(host);
+
+    builder.setHttpClientConfigCallback(clientBuilder -> {
+      //clientBuilder.setSSLContext(TestElasticsearchSuite.createContextFromCaCert(getCertAsBytes(elasticsearch)));
+      clientBuilder.setDefaultCredentialsProvider(getCredentialsProvider());
+      return clientBuilder;
+    });
+
+    ElasticsearchTransport transport = new RestClientTransport(
+        builder.build(), new JacksonJsonpMapper());
+
+    return new ElasticsearchClient(transport);
+  }
+
+
+
+  private static void startElasticsearch() throws GeneralSecurityException {
+    elasticsearch = new ElasticsearchContainer(IMAGE_NAME)
+        .withExposedPorts(9200)
+        .withStartupTimeout(Duration.ofMinutes(2))
+        .withStartupAttempts(5)
+        .withPassword(ELASTICSEARCH_PASSWORD)
+        .withEnv("xpack.security.enabled", "true")
+        .withEnv("xpack.security.transport.ssl.enabled", "false")
+        .withEnv("ES_JAVA_OPTS", "-Xmx1g"); // ES gobbles up lots of RAM under defaults.
+
+    HttpsURLConnection.setDefaultSSLSocketFactory(SslUtils.trustAllSSLContext().getSocketFactory());
+    elasticsearch.start();
   }
 
   public static String getAddress() {
