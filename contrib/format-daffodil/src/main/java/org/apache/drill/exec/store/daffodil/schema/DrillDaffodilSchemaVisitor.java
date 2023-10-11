24e9b7ed40cba14fc99da41ab18d1d16b057eded
Michael Beckerle <mbeckerle@apache.org>
2023-10-11 08:21:17 -0400

    Adding Daffodil to Drill as a 'contrib'
    Requires Daffodil version 3.7.0 or higher.
    
    New format-daffodil module created
    
    Still uses absolute paths for the schemaFileURI.
    (which is cheating. Wouldn't work in a true distributed
    drill environment.)
    
    We have yet to work out how to enable Drill to provide
    access for DFDL schemas in XML form with include/import
    to be resolved.
    
    The input data stream is, however, being accessed in the
    proper Drill manner. Gunzip happened automatically. Nice.
    
    Note: Fix boxed Boolean vs. boolean problem. Don't use
    boxed primitives in Format config objects.
    
    Test show this works for data as complex as having
    nested repeating sub-records.
    
    These DFDL types are supported:
    
    - int
    - long
    - short
    - byte
    - boolean
    - double
    - float (does not work. Bug DAFFODIL-2367)
    - hexBinary
    - string
    
    https://github.com/apache/drill/issues/2835

---
+++ contrib/format-daffodil/src/main/java/org/apache/drill/exec/store/daffodil/schema/DrillDaffodilSchemaVisitor.java
@@ -0,0 +1,128 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.drill.exec.store.daffodil.schema;
+
+import org.apache.daffodil.runtime1.api.ChoiceMetadata;
+import org.apache.daffodil.runtime1.api.ComplexElementMetadata;
+import org.apache.daffodil.runtime1.api.ElementMetadata;
+import org.apache.daffodil.runtime1.api.MetadataHandler;
+import org.apache.daffodil.runtime1.api.SequenceMetadata;
+import org.apache.daffodil.runtime1.api.SimpleElementMetadata;
+import org.apache.drill.common.types.TypeProtos.MinorType;
+import org.apache.drill.exec.record.metadata.MapBuilderLike;
+import org.apache.drill.exec.record.metadata.SchemaBuilder;
+import org.apache.drill.exec.record.metadata.TupleMetadata;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Stack;
+
+/**
+ * This class transforms a DFDL/Daffodil schema into a Drill Schema.
+ */
+public class DrillDaffodilSchemaVisitor extends MetadataHandler {
+
+  private static final Logger logger = LoggerFactory.getLogger(DrillDaffodilSchemaVisitor.class);
+
+  /**
+   * SchemaBuilder and MapBuilder share a polymorphic interface MapBuilderLike
+   */
+  private final SchemaBuilder builder = new SchemaBuilder();
+  private final Stack<MapBuilderLike> mapBuilderStack = new Stack<>();
+
+  private MapBuilderLike mapBuilder() {
+    return mapBuilderStack.peek();
+  }
+
+  /**
+   * Converts Daffodil names into appropriate Drill column names.
+   * @param md Daffodil element metadata, which contains an element name.
+   * @return a string usable as a Drill column name
+   */
+  public static String makeColumnName(ElementMetadata md) {
+    return md.toQName().replace(":", "_");
+  }
+
+  /**
+   * Returns a {@link TupleMetadata} representation of the DFDL schema. Should only be called after
+   * the walk of the DFDL schema with this visitor has been called.
+   *
+   * @return A {@link TupleMetadata} representation of the DFDL schema.
+   */
+  public TupleMetadata getDrillSchema() {
+    return builder.build();
+  }
+
+  @Override
+  public void simpleElementMetadata(SimpleElementMetadata md) {
+    assert (!mapBuilderStack.isEmpty());
+    String colName = makeColumnName(md);
+    MinorType drillType = DrillDaffodilSchemaUtils.getDrillDataType(md.dfdlType());
+    if (md.isArray()) {
+      mapBuilder().addArray(colName, drillType);
+    } else if (md.isOptional() || md.isNillable()) {
+      mapBuilder().addNullable(colName, drillType);
+    } else {
+      mapBuilder().add(colName, drillType);
+    }
+  }
+
+  @Override
+  public void startComplexElementMetadata(ComplexElementMetadata md) {
+    if (mapBuilderStack.isEmpty()) {
+      // root element case. The SchemaBuilder top level row is the container of the root element's children
+      mapBuilderStack.push(builder);
+    } else {
+      // enclosed complex element case. Create a map field.
+      String colName = makeColumnName(md);
+      if (md.isArray()) {
+        mapBuilderStack.push(mapBuilder().addMapArray(colName));
+      } else {
+        mapBuilderStack.push(mapBuilder().addMap(colName)); // also handles optional complex elements
+      }
+    }
+  }
+
+  @Override
+  public void endComplexElementMetadata(ComplexElementMetadata md) {
+    assert (!mapBuilderStack.isEmpty());
+    mapBuilder().resume();
+    mapBuilderStack.pop();
+  }
+
+  @Override
+  public void startSequenceMetadata(SequenceMetadata m) {
+  }
+
+  @Override
+  public void endSequenceMetadata(SequenceMetadata m) {
+  }
+
+  @Override
+  public void startChoiceMetadata(ChoiceMetadata m) {
+  }
+
+  @Override
+  public void endChoiceMetadata(ChoiceMetadata m) {
+  }
+
+  private void DFDLSchemaError(String s) {
+    throw new RuntimeException(s);
+  }
+}
