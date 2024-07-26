6b91c4c9a9d6c5cf93eae6da6103507fada8f725
Charles Givre <cgivre@apache.org>
2024-07-26 13:42:08 -0400

    Added config options

--- contrib/storage-splunk/src/test/java/org/apache/drill/exec/store/splunk/SplunkConnectionTest.java
+++ contrib/storage-splunk/src/test/java/org/apache/drill/exec/store/splunk/SplunkConnectionTest.java
@@ -59,7 +59,7 @@ public class SplunkConnectionTest extends SplunkBaseTest {
         SPLUNK_STORAGE_PLUGIN_CONFIG.getLatestTime(),
         null,
         SPLUNK_STORAGE_PLUGIN_CONFIG.getReconnectRetries(),
-        StoragePluginConfig.AuthMode.SHARED_USER.name(), true, null
+        StoragePluginConfig.AuthMode.SHARED_USER.name(), true, null, null, null, null
       );
       SplunkConnection sc = new SplunkConnection(invalidSplunkConfig, null);
       sc.connect();
