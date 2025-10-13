b1e6b548f60251749dbf9e7ffa4e36bb908cd4b1
cgivre <cgivre@gmail.com>
2025-10-12 21:36:44 -0400

    Bump Calcite to version 1.36

--- exec/java-exec/src/main/java/org/apache/drill/exec/planner/sql/conversion/SqlConverter.java
+++ exec/java-exec/src/main/java/org/apache/drill/exec/planner/sql/conversion/SqlConverter.java
@@ -274,7 +274,7 @@ public class SqlConverter {
 
       RelNode relNode = rel.rel;
       List<RexNode> expressions = rel.fields.stream()
-          .map(f -> builder.makeInputRef(relNode, f.left))
+          .map(f -> builder.makeInputRef(relNode, f.getKey()))
           .collect(Collectors.toList());
 
       RelNode project = LogicalProject.create(rel.rel, Collections.emptyList(), expressions, rel.validatedRowType);
