61a1566efe13c60f1fa381f6e149dcc961602b1b
Charles Givre <cgivre@apache.org>
2026-08-02 05:21:02 -0700

    Fix test issues

--- contrib/storage-accumulo/src/test/java/org/apache/drill/exec/store/accumulo/AccumuloFilterBuilderTest.java
+++ contrib/storage-accumulo/src/test/java/org/apache/drill/exec/store/accumulo/AccumuloFilterBuilderTest.java
@@ -19,8 +19,6 @@ package org.apache.drill.exec.store.accumulo;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
 
 import java.nio.charset.StandardCharsets;
